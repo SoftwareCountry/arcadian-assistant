05645830195cf4e03c4a80ff05a6fce1f72c75f3
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2017-10-30 20:43:48 +0300

    Renamed files

--- artifacts/App.js
+++ artifacts/App.js
@@ -6,7 +6,7 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' +
+    ios: 'Press Cmd+R to  reload,\n' +
         'Cmd+D or shake for dev menu',
     android: 'Double tap R on your keyboard to reload,\n' +
         'Shake or press menu button for dev menu',
@@ -37,4 +37,4 @@ const styles = StyleSheet.create({
         marginBottom: 5,
     },
 });
-//# sourceMappingURL=App.js.map
+//# sourceMappingURL=app.js.map
