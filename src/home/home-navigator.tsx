c3b85eb1c293bbe925eefe5ad27866a2920c6d8d
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2017-11-03 22:20:33 +0300

    starting to work on home navigator

--- src/home/home-navigator.tsx
+++ src/home/home-navigator.tsx
@@ -1,1 +1,9 @@
-//const Home
+import React from 'react';
+import { StackNavigator } from 'react-navigation';
+import { Text } from 'react-native';
+
+const HomeNavigator = StackNavigator( {
+    Sicklist: () => <Text>sicklist</Text>,
+    Vacations: () => <Text>vacactions</Text>,
+    Actions: () => <Text>actions</Text>
+});
