a5125043a0325dc386c9b571aaa66ddf38fbc555
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2017-11-04 19:27:05 +0300

    Added console app and main library

---
+++ Arcadia.Assistant.Server.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Arcadia.Assistant.Server.Console
+{
+    using System;
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
