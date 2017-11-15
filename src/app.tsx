f5a00032c3f570af6362ccc1e91d04477ae81766
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2017-11-15 19:01:22 +0300

    Added Back button handling for Android

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, BackHandler } from 'react-native';
 
 import { RootNavigator } from './layout/root-navigator';
 import { AppState, storeFactory } from './reducers/app.reducer';
-import { connect, Provider } from 'react-redux';
-import { addNavigationHelpers } from 'react-navigation';
+import { connect, Provider, Dispatch } from 'react-redux';
+import { addNavigationHelpers, NavigationState, NavigationActions } from 'react-navigation';
 
-export class App extends Component<any> {
+export class App extends Component<{ dispatch: any, nav: NavigationState }> {
+
+  public componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.onBackPress );
+  }
+
+  public componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
+  }
+
   public render() {
     return <RootNavigator navigation={addNavigationHelpers({
       dispatch: this.props.dispatch,
       state: this.props.nav
     })} />;
+  }
+
+  private onBackPress() {
+    const { dispatch, nav } = this.props;
+    if (nav.index === 0) {
+      return false;
+    }
+
+    dispatch(NavigationActions.back());
+    return true;
   }
 }
 
