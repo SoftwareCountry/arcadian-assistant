55b33dfeb57b34d522a7e104459395b2d21f4b84
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2017-11-25 17:46:16 +0300

    Use Ctrl+C instead of ReadKey. Switched to Linux containers

--- server/Arcadia.Assistant.Server.Console/Program.cs
+++ server/Arcadia.Assistant.Server.Console/Program.cs
@@ -1,16 +1,29 @@
 ﻿namespace Arcadia.Assistant.Server.Console
 {
     using System;
+    using System.Threading;
+    using System.Threading.Tasks;
 
     internal class Program
     {
+        private static readonly AutoResetEvent closing = new AutoResetEvent(false);
+
         public static void Main(string[] args)
         {
+            //var config = new ConfigurationBuilder
+
             using (var app = new Application())
             {
                 app.Start();
-                Console.ReadKey();
+                Console.CancelKeyPress += OnExit;
+                closing.WaitOne();
             }
+        }
+
+        private static void OnExit(object sender, ConsoleCancelEventArgs args)
+        {
+            Console.WriteLine("Exiting...");
+            closing.Set();
         }
     }
 }
