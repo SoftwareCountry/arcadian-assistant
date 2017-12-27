fcd4aeb6e64b2f317f4d9bb0f9374ce0b41ce133
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2017-12-27 17:19:26 +0300

    Wrote some tests, fixed some bugs related to employees loading

--- mobile/src/reducers/organization/organization.epics.ts
+++ mobile/src/reducers/organization/organization.epics.ts
@@ -9,24 +9,32 @@ import { Employee } from './employee.model';
 const url = 'http://localhost:5000/api'; //TODO: fix hardcode
 
 export const loadDepartmentsEpic$ = (action$: ActionsObservable<LoadDepartments>) =>
-action$.ofType('LOAD-DEPARTMENTS')
-    .switchMap(x => ajaxGetJSON(`${url}/departments`))
-    .map(x => deserializeArray(x as any, Department))
-    .map(x => loadDepartmentsFinished(x));
+    action$.ofType('LOAD-DEPARTMENTS')
+        .switchMap(x => ajaxGetJSON(`${url}/departments`))
+        .map(x => deserializeArray(x as any, Department))
+        .map(x => loadDepartmentsFinished(x));
 
 export const loadChiefsEpic$ = (action$: ActionsObservable<LoadDepartmentsFinished> ) => 
-action$.ofType('LOAD-DEPARTMENTS-FINISHED')
-    .switchMap(x => x.departments.map(dep => ajaxGetJSON(`${url}/employees/${dep.chiefId}`).map(obj => deserialize(obj, Employee))))
-    .mergeAll()
-    .map(x => loadEmployeeFinished(x));
+    action$.ofType('LOAD-DEPARTMENTS-FINISHED')
+        .switchMap(x => 
+            x.departments.map(dep =>
+                ajaxGetJSON(`${url}/employees/${dep.chiefId}`).map(obj => deserialize(obj, Employee))))
+        .mergeAll()
+        .map(x => loadEmployeeFinished(x));
 
+
+//TODO: this thing loads all employees for all departments. It needs to be changed to load only requested ones
 export const loadDepartmentsFinishedEpic$ = (action$: ActionsObservable<LoadDepartmentsFinished> ) =>     
-action$.ofType('LOAD-DEPARTMENTS-FINISHED')
-    .flatMap(x => x.departments.map(x => loadEmployeesForDepartment(x.departmentId)));
+    action$.ofType('LOAD-DEPARTMENTS-FINISHED')
+        .flatMap(x => 
+            x.departments.map(dep => 
+                loadEmployeesForDepartment(dep.departmentId)));
 
 export const loadEmployeesForDepartmentEpic$ = (action$: ActionsObservable<LoadEmployeesForDepartment>, state: AppState) =>
-action$.ofType('LOAD_EMPLOYEES_FOR_DEPARTMENT')
-    .groupBy(x => x.departmentId)
-    .map(x => x.switchMap( y => ajaxGetJSON(`${url}/employees?departmentId=${x.key}`).map(obj => deserializeArray(obj as any, Employee))))
-    .mergeAll()
-    .flatMap(x => x.map(loadEmployeeFinished));
+    action$.ofType('LOAD_EMPLOYEES_FOR_DEPARTMENT')
+        .groupBy(x => x.departmentId)
+        .map(x => 
+            x.switchMap(y =>
+                ajaxGetJSON(`${url}/employees?departmentId=${x.key}`).map(obj => deserializeArray(obj as any, Employee))))
+        .mergeAll()
+        .flatMap(x => x.map(loadEmployeeFinished));
