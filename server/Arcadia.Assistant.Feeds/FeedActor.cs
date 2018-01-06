da7e76e04a3778373348cff2e323ca27c6394860
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2018-01-07 01:26:41 +0300

    Added simple message feed as POC

---
+++ server/Arcadia.Assistant.Feeds/FeedActor.cs
@@ -0,0 +1,60 @@
+﻿namespace Arcadia.Assistant.Feeds
+{
+    using System.Collections.Generic;
+
+    using Akka.Persistence;
+    public class FeedActor : UntypedPersistentActor
+    {
+        private readonly List<Message> messagesList = new List<Message>();
+
+        public FeedActor(string feedName)
+        {
+            this.PersistenceId = feedName;
+        }
+
+        public override string PersistenceId { get; }
+
+        protected override void OnCommand(object cmd)
+        {
+            switch (cmd)
+            {
+                case PostMessage postMessage:
+                    var newEvent = new MessageIsPostedToFeedEvent()
+                        {
+                            MessageId = postMessage.Message.MessageId,
+                            PostedDate = postMessage.Message.DatePosted,
+                            Text = postMessage.Message.Text,
+                            Title = postMessage.Message.Title
+                        };
+
+                    this.Persist(newEvent, this.MessagePosted);
+                    break;
+            }
+        }
+
+        protected override void OnRecover(object message)
+        {
+            switch (message)
+            {
+                case MessageIsPostedToFeedEvent e:
+                    this.MessagePosted(e);
+                    break;
+            }
+        }
+
+        private void MessagePosted(MessageIsPostedToFeedEvent e)
+        {
+            this.messagesList.Add(new Message(e.MessageId, e.Title, e.Text, e.PostedDate));
+        }
+
+        public sealed class PostMessage
+        {
+            public Message Message { get; }
+
+            public PostMessage(Message message)
+            {
+                this.Message = message;
+            }
+        }
+    }
+}
---
+++ server/Arcadia.Assistant.Feeds/MessageIsPostedToFeedEvent.cs
@@ -0,0 +1,21 @@
+﻿namespace Arcadia.Assistant.Feeds
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public sealed class MessageIsPostedToFeedEvent
+    {
+        [DataMember]
+        public Guid MessageId { get; set; }
+
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public string Text { get; set; }
+
+        [DataMember]
+        public DateTime PostedDate { get; set; }
+    }
+}
