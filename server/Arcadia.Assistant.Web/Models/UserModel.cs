b52ba6bc720283de52810df727cf7f5a14a80ead
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2018-01-06 19:20:28 +0300

    added mock user endpoing

---
+++ server/Arcadia.Assistant.Web/Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Arcadia.Assistant.Web.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+
+        public string EmployeeId { get; set; }
+    }
+}
