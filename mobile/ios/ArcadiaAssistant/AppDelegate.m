c772d265257218a323b7573ea7653094c717d361
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2018-01-15 17:10:12 +0300

    Set up arcadia-assistant schema for both iOS and Android applications

--- mobile/ios/ArcadiaAssistant/AppDelegate.m
+++ mobile/ios/ArcadiaAssistant/AppDelegate.m
@@ -11,6 +11,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <React/RCTLinkingManager.h>
 
 @implementation AppDelegate
 
@@ -32,6 +33,13 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
 }
 
 @end
