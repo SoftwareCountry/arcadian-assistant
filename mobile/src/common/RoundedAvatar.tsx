76240493c32f6f643160d3a3e7546f4fcb111023
Victor Kotov <victor.kotov@arcadia.spb.ru>
2018-01-17 12:10:07 +0300

    RoundedAvatar component added
    Properties list:
    photoBase64 - Base64 encoded photo
    outerBorderColor - blue border around the image (according to current
    design)
    innerBorderColor - color of the “fake” border applied to the image
    (superview’s background according to current design)
    avatarSize - width&height of avatar
    outerBorderWidth
    innerBorderWidth

---
+++ mobile/src/common/RoundedAvatar.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { View, Image } from 'react-native';
+
+export interface Props {
+    photoBase64: string;
+    outerBorderColor: string;
+    innerBorderColor: string;
+    avatarSize: number;
+    outerBorderWidth: number;
+    innerBorderWidth: number;
+}
+
+export class RoundedAvatar extends Component<Props> {
+    public render() {
+        const { outerBorderColor, innerBorderColor, photoBase64, avatarSize, outerBorderWidth, innerBorderWidth } = this.props;
+        const imageSize = avatarSize - outerBorderWidth;
+
+        return (
+            <View style={{ height: avatarSize, width: avatarSize, justifyContent: 'center', alignItems: 'center', borderRadius: avatarSize * 0.5, borderColor: outerBorderColor, borderWidth: outerBorderWidth }}>
+                <Image source={{uri: photoBase64}} style={{ height: imageSize, width: imageSize, borderRadius: imageSize * 0.5, borderColor: innerBorderColor, borderWidth: innerBorderWidth }} />
+            </View>
+        );
+    }
+}
