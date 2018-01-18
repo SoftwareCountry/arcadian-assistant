6a308377bfc73a561e1f6575591e20b2fda17699
Tsvetkova, Elena <elena.tsvetkova@arcadia.spb.ru>
2018-01-18 11:56:50 +0300

    Clean up.

--- mobile/src/feeds/feeds-screen.tsx
+++ mobile/src/feeds/feeds-screen.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 
 import { StackNavigator } from 'react-navigation';
 import { HomeFeedsScreen } from './home-feeds-screen';
-import { SicklistScreen } from '../home/sicklist-screen';
-import { HelpdeskScreen } from '../home/helpdesk/helpdesk-screen';
-import { DaysoffScreen } from '../home/daysoff-screen';
-import { VacationsScreen } from '../home/vacations-screen';
 
 export const FeedsScreen = StackNavigator({
     Home: {
