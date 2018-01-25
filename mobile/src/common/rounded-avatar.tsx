f830c83c70366b92c87c08a9c1c1d84457d47764
Victor Kotov <victor.kotov@arcadia.spb.ru>
2018-01-25 15:40:55 +0300

    Fixes by PR comments for avatar component

--- mobile/src/common/rounded-avatar.tsx
+++ mobile/src/common/rounded-avatar.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, Image } from 'react-native';
 
-export interface Props {
+export interface RoundedAvatarProps {
     photoBase64: string;
     outerBorderColor: string;
     // Inner border color could be same as superview's background color 
@@ -12,14 +12,14 @@ export interface Props {
     innerBorderWidth: number;
 }
 
-export class RoundedAvatar extends Component<Props> {
+export class RoundedAvatar extends Component<RoundedAvatarProps> {
     public render() {
         const { outerBorderColor, innerBorderColor, photoBase64, avatarSize, outerBorderWidth, innerBorderWidth } = this.props;
         const imageSize = avatarSize - outerBorderWidth;
 
         return (
             <View style={{ height: avatarSize, width: avatarSize, justifyContent: 'center', alignItems: 'center', borderRadius: avatarSize * 0.5, borderColor: outerBorderColor, borderWidth: outerBorderWidth }}>
-                <Image source={{uri: photoBase64}} style={{ height: imageSize, width: imageSize, borderRadius: imageSize * 0.5, borderColor: innerBorderColor, borderWidth: innerBorderWidth }} />
+                <Image source={{uri: 'data:image/png;base64,' + photoBase64}} style={{ height: imageSize, width: imageSize, borderRadius: imageSize * 0.5, borderColor: innerBorderColor, borderWidth: innerBorderWidth }} />
             </View>
         );
     }
