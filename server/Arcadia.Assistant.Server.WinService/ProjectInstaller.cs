1b3cf6755a45a54d9bdfa175ea1681aa5904d9c6
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2018-01-28 01:43:58 +0300

    Added winservice installer

---
+++ server/Arcadia.Assistant.Server.WinService/ProjectInstaller.cs
@@ -0,0 +1,13 @@
+﻿namespace Arcadia.Assistant.Server.WinService
+{
+    using System.ComponentModel;
+
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
