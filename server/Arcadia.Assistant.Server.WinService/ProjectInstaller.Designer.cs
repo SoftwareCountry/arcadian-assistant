2a58269d8d319e663ab4570f76c0040a4d315db9
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2018-01-28 03:01:21 +0300

    Network service account is used by default

--- server/Arcadia.Assistant.Server.WinService/ProjectInstaller.Designer.cs
+++ server/Arcadia.Assistant.Server.WinService/ProjectInstaller.Designer.cs
@@ -33,6 +33,7 @@
             // 
             // serviceProcessInstaller
             // 
+            this.serviceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.NetworkService;
             this.serviceProcessInstaller.Password = null;
             this.serviceProcessInstaller.Username = null;
             // 
