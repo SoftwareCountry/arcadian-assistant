fcef2a9c264713764e2a4c01f54320daabbf1126
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2018-01-28 15:05:35 +0300

    Added proper logging to Web

--- server/Arcadia.Assistant.Web/Program.cs
+++ server/Arcadia.Assistant.Web/Program.cs
@@ -1,15 +1,28 @@
 ﻿namespace Arcadia.Assistant.Web
 {
+    using System;
+
     using Autofac.Extensions.DependencyInjection;
 
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
+    using NLog.Web;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var logger = NLogBuilder.ConfigureNLog("Nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Info("Starting up...");
+                BuildWebHost(args).Run();
+            }
+            catch (Exception e)
+            {
+                logger.Fatal(e, "The program failed to start");
+                throw;
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
@@ -17,6 +30,7 @@
                 .ConfigureServices(services => services.AddAutofac())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseNLog()
                 .Build();
     }
 }
