42bfd29a777ad98b3def604b45f7b63e677e3f2b
Alexander Shevnin <santee@users.noreply.github.com>
2018-01-31 00:09:27 +0300

    conditional jsCodeLocation, using jsbundle for Release configuration

--- mobile/ios/ArcadiaAssistant/AppDelegate.m
+++ mobile/ios/ArcadiaAssistant/AppDelegate.m
@@ -19,7 +19,11 @@
 {
   NSURL *jsCodeLocation;
 
+  #if DEBUG  
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  #else
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  #endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"ArcadiaAssistant"
