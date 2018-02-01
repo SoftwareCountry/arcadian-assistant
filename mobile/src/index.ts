b01966f084d545972688cf3316d789bf591bd0db
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2018-02-01 18:26:13 +0300

    Fixed unhandled error in users loading. Adde default global error handled

--- mobile/src/index.ts
+++ mobile/src/index.ts
@@ -1,4 +1,10 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Alert } from 'react-native';
 import { Root } from './app';
+
+const defaultHandler = ErrorUtils.getGlobalHandler && ErrorUtils.getGlobalHandler();
+ErrorUtils.setGlobalHandler(function(error: any, isFatal: true) {
+    console.error("Unhandled error ocurred");
+    defaultHandler.apply(this, arguments);
+});
 
 AppRegistry.registerComponent('ArcadiaAssistant', () => Root);
