7da875803b67432531297b0759665997761563f3
sergey.kim <sergey.kim@arcadia.spb.ru>
2018-02-01 14:49:19 +0300

    added stacknavigator

--- mobile/src/calendar/calendar-screen.tsx
+++ mobile/src/calendar/calendar-screen.tsx
@@ -1,15 +1,10 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import styles from '../layout/styles';
-import { calendarScreenLayout } from './styles';
-import { Calendar } from './calendar';
-import { Agenda } from './agenda';
+import { StackNavigator } from 'react-navigation';
+import React from 'react';
+import { CalendarScreenImpl } from './home-calendar-screen';
 
-export class CalendarScreen extends Component {
-    public render() {
-        return <View style={styles.container}>
-            <Calendar />
-            <Agenda />
-        </View>;
-    }
-}
+export const CalendarScreen = StackNavigator({
+    Home: {
+        screen: CalendarScreenImpl,
+        path: '/',
+    },
+});
