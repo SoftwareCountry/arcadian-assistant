eff3620705cf9204ff869222f2ab544d0d1665cd
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2018-02-01 18:30:31 +0300

    Fixed tslint errors

--- mobile/src/index.ts
+++ mobile/src/index.ts
@@ -3,8 +3,8 @@ import { Root } from './app';
 
 const defaultHandler = ErrorUtils.getGlobalHandler && ErrorUtils.getGlobalHandler();
 ErrorUtils.setGlobalHandler(function(error: any, isFatal: true) {
-    console.error("Unhandled error ocurred");
-    defaultHandler.apply(this, arguments);
+    console.error('Unhandled error ocurred');
+    defaultHandler(error, isFatal);
 });
 
 AppRegistry.registerComponent('ArcadiaAssistant', () => Root);
