b19b3243fff919d204f5d6cfe0f9b2b53f3bc16a
sergey.kim <sergey.kim@arcadia.spb.ru>
2018-02-05 18:09:43 +0300

    Calendar: display today

--- mobile/src/calendar/calendar.tsx
+++ mobile/src/calendar/calendar.tsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, ViewStyle } from 'react-native';
 import { calendarScreenLayout, calendarStyles } from './styles';
+import moment from 'moment';
 
 export class Calendar extends Component {
     public render() {
+        // TODO: temp
+        const today = moment();
+
         return (
             <View style={calendarStyles.container}>
-                <Text style={calendarStyles.containerTitle}>January 2018</Text>
+                <Text style={calendarStyles.containerTitle}>{today.format('MMMM YYYY')}</Text>
                 <View></View>
             </View>
         );
