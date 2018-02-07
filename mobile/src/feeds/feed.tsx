56a8b0421dfa0cc98b18d4c4cab15dd5089233f7
Dementiy Kalkov <dementiy.kalkov@arcadia.spb.ru>
2018-02-07 15:53:32 +0300

    format `datePosted` on render

--- mobile/src/feeds/feed.tsx
+++ mobile/src/feeds/feed.tsx
@@ -12,41 +12,17 @@ interface FeedListItemProps {
     employee: Employee;
 }
 
-interface FeedListItemState {
-    imgContainerSize?: number;
-    formattedDate?: string;
-}
-
-export class FeedListItem extends React.Component<FeedListItemProps, FeedListItemState> {
-    constructor(props: FeedListItemProps) {
-        super(props);
-        this.state = {};
-    }
-
-    public componentWillReceiveProps(nextProps: Readonly<FeedListItemProps>) {
-        const nextDate = nextProps.message ? nextProps.message.datePosted : null;
-        const datePosted = this.props.message ? this.props.message.datePosted : null;
-
-        if (datePosted !== nextDate || !this.state.formattedDate) {
-            this.setState({
-                formattedDate: nextDate.format('MMMM D, YYYY')
-            });
-        }
-    }
-
+export class FeedListItem extends React.Component<FeedListItemProps> {
     public render() {
         const message = this.props.message;
         const employee = this.props.employee;
-
-        const imgStyle = StyleSheet.flatten([{
-            width: this.state.imgContainerSize,
-            height: this.state.imgContainerSize
-        }]);
 
         const employeeName = employee ? employee.name : message.title;
         const photo = employee ? employee.photo : null;
         const mimeType = photo ? photo.mimeType : null;
         const base64 = photo ? photo.base64 : null;
+
+        const formattedDate = message.datePosted.format('MMMM D, YYYY');
 
         return (
             <TouchableHighlight>
@@ -58,7 +34,7 @@ export class FeedListItem extends React.Component<FeedListItemProps, FeedListIte
                         <Text style={styles.title}>{employeeName}</Text>
                         <Text style={styles.text}>{this.props.message.text}</Text>
                         <Text style={styles.tags}>#ArcadiaNews</Text>
-                        <Text style={styles.date}>{this.state.formattedDate}</Text>
+                        <Text style={styles.date}>{formattedDate}</Text>
                     </View>
                 </View>
             </TouchableHighlight>
