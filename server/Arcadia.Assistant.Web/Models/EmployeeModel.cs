09e9ae09c277beb1a00703c62ad0ace31909bec5
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2018-02-08 14:32:43 +0300

    Moved base64 conversion to the separate model for optimization reasons

--- server/Arcadia.Assistant.Web/Models/EmployeeModel.cs
+++ server/Arcadia.Assistant.Web/Models/EmployeeModel.cs
@@ -14,7 +14,7 @@
 
         public Sex Sex { get; set; }
 
-        public Photo Photo { get; set; }
+        public PhotoModel Photo { get; set; }
 
         public string Position { get; set; }
 
---
+++ server/Arcadia.Assistant.Web/Models/PhotoModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Arcadia.Assistant.Web.Models
+{
+    using System;
+
+    using Arcadia.Assistant.Organization.Abstractions;
+
+    public class PhotoModel
+    {
+        public string MimeType { get; set; }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public string Bytes { get; set; }
+
+        public static implicit operator PhotoModel(Photo photo)
+        {
+            return new PhotoModel()
+                {
+                    MimeType = photo.MimeType,
+                    Bytes = Convert.ToBase64String(photo.Bytes.ToArray()), //TODO: wait for .net 2.1 to change on System.Buffers.Text
+                    Height = photo.Height,
+                    Width = photo.Width
+                };
+        }
+    }
+}
