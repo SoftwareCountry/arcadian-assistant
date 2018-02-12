3a2830c78eecd4fb040d3c3f512d9ee062adf076
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2018-02-12 16:39:04 +0300

    Renamed Bytes back to Base64

--- server/Arcadia.Assistant.Web/Models/PhotoModel.cs
+++ server/Arcadia.Assistant.Web/Models/PhotoModel.cs
@@ -12,14 +12,14 @@
 
         public int Height { get; set; }
 
-        public string Bytes { get; set; }
+        public string Base64 { get; set; }
 
         public static implicit operator PhotoModel(Photo photo)
         {
             return new PhotoModel()
                 {
                     MimeType = photo.MimeType,
-                    Bytes = Convert.ToBase64String(photo.Bytes), //TODO: wait for .net 2.1 to change on System.Buffers.Text
+                    Base64 = Convert.ToBase64String(photo.Bytes), //TODO: wait for .net 2.1 to change on System.Buffers.Text
                     Height = photo.Height,
                     Width = photo.Width
                 };
