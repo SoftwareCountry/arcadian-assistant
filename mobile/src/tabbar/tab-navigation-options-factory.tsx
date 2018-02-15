e1f97b358b607122b080815212521d3157544f94
Tsvetkova, Elena <elena.tsvetkova@arcadia.spb.ru>
2018-02-15 15:25:58 +0300

    Rename ApplicationIcon, removed .png icons, using icon fonts for profile screen, replace styles from profile-screen to separate file, phone icon on profile screen is temporary.

--- mobile/src/tabbar/tab-navigation-options-factory.tsx
+++ mobile/src/tabbar/tab-navigation-options-factory.tsx
@@ -3,14 +3,14 @@ import { NavigationTabScreenOptions } from 'react-navigation';
 import {Platform, Dimensions } from 'react-native';
 import tabBarStyles from './tab-bar-styles';
 import { StyledText } from '../override/styled-text';
-import { ApplicationTabbarIcons } from '../override/application-tabBar-icons';
+import { ApplicationIcon } from '../override/application-icon';
 
 export class TabNavigationOptionsFactory {
     public create(label: string, iconName: string): NavigationTabScreenOptions {
         return {
             tabBarLabel: this.getTabBarLabel(label),
             tabBarIcon: ({ tintColor, focused }) =>
-           <ApplicationTabbarIcons name={iconName} size={Platform.OS === 'ios' ? Dimensions.get('window').width * 0.08 : Dimensions.get('window').width * 0.04 } style = {tabBarStyles.tabImages} ></ApplicationTabbarIcons>
+           <ApplicationIcon name={iconName} size={Platform.OS === 'ios' ? Dimensions.get('window').width * 0.08 : Dimensions.get('window').width * 0.04 } style = {tabBarStyles.tabImages} ></ApplicationIcon>
         };
     }
 
