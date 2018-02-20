f3ad5272749fea84bca961ecd674ff04ed461252
Dementiy Kalkov <dementiy.kalkov@arcadia.spb.ru>
2018-02-20 12:19:45 +0300

    show only visible event types on a legend

--- mobile/src/calendar/calendar-legend.tsx
+++ mobile/src/calendar/calendar-legend.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, PixelRatio } from 'react-native';
 import { connect } from 'react-redux';
-import { IntervalsModel } from '../reducers/calendar/calendar.model';
+import { IntervalsModel, WeekModel } from '../reducers/calendar/calendar.model';
 import { AppState } from '../reducers/app.reducer';
 import { CalendarEventsType } from '../reducers/calendar/calendar-events.model';
 import { calendarIntervalColors } from './styles';
@@ -9,10 +9,12 @@ import { StyledText } from '../override/styled-text';
 
 interface CalendarLegendProps {
     intervals: IntervalsModel;
+    weeks: WeekModel[];
 }
 
 const mapStateToProps = (state: AppState): CalendarLegendProps => ({
     intervals: state.calendar.calendarEvents.intervals,
+    weeks: state.calendar.calendarEvents.weeks
 });
 
 const legendMarkerSize = 12;
@@ -41,30 +43,53 @@ const styles = StyleSheet.create({
 });
 
 class CalendarLegendImpl extends Component<CalendarLegendProps> {
-    private readonly intervalColors = {
+    private readonly intervalColors: { [type: string]: string } = {
         [CalendarEventsType.Vacation]: calendarIntervalColors.vacation,
         [CalendarEventsType.SickLeave]: calendarIntervalColors.sickLeave,
         [CalendarEventsType.Dayoff]: calendarIntervalColors.dayoff
     };
 
-    private readonly intervalNames = {
+    private readonly intervalNames: { [type: string]: string } = {
         [CalendarEventsType.Vacation]: 'Vacation',
         [CalendarEventsType.SickLeave]: 'Sick Leave',
         [CalendarEventsType.Dayoff]: 'Dayoff'
     };
 
-    public render() {
-        const { intervals } = this.props;
+    private getVisibleCalendarEvents() {
+        const { intervals, weeks } = this.props;
+        const calendarEventsMap: Map<CalendarEventsType, number> = new Map();
 
-        const existingIntervals = [CalendarEventsType.Vacation, CalendarEventsType.SickLeave, CalendarEventsType.Dayoff];
-        const legend = existingIntervals.map(type => {
+        if (!weeks || !intervals) {
+            return calendarEventsMap;
+        }
+
+        weeks.forEach(week => {
+            week.days.forEach(day => {
+                const dayIntervals = intervals.get(day.date) || [];
+                dayIntervals.forEach(interval => {
+                    let eventCounter = calendarEventsMap.get(interval.eventType) || 0;
+                    calendarEventsMap.set(interval.eventType, eventCounter++);
+                });
+            });
+        });
+
+        return calendarEventsMap;
+    }
+
+    public render() {
+        const { intervals, weeks } = this.props;
+
+        const existingIntervals = this.getVisibleCalendarEvents();
+        const legend: JSX.Element[] = [];
+
+        existingIntervals.forEach((value, type) => {
             const style = StyleSheet.flatten([styles.marker, { backgroundColor: this.intervalColors[type] }]);
-            return (
+            legend.push((
                 <View key={type} style={styles.itemContainer}>
                     <View style={style}></View>
                     <StyledText style={styles.label}>{this.intervalNames[type]}</StyledText>
                 </View>
-            );
+            ));
         });
 
         return (
