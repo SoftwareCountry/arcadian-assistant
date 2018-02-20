68b697fe43be7029b2da814cd1ed7607c04fa88a
Victor Kotov <victor.kotov@arcadia.spb.ru>
2018-02-20 17:43:59 +0300

    Merge conflicts resolve

--- mobile/src/tabbar/tab-navigator.tsx
+++ mobile/src/tabbar/tab-navigator.tsx
@@ -12,12 +12,8 @@ const tabNavigatorConfig: TabNavigatorConfig = {
         labelStyle: tabBarStyles.tabBarLabel,
         showIcon: true,
     },
-<<<<<<< HEAD
     animationEnabled: false,
     lazy: true
-=======
-    animationEnabled: false
->>>>>>> 30bb38a237a06b225a4e2ded8f6d85dddf5defe9
 };
 
 export const RootNavigator = TabNavigator(
