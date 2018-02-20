854f66e3c876984acc5637e41ee5f9aba20ce84e
Dementiy Kalkov <dementiy.kalkov@arcadia.spb.ru>
2018-02-20 16:26:51 +0300

    show legend for selected date only

--- mobile/src/calendar/calendar-legend.tsx
+++ mobile/src/calendar/calendar-legend.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, PixelRatio } from 'react-native';
 import { connect } from 'react-redux';
-import { IntervalsModel, WeekModel } from '../reducers/calendar/calendar.model';
+import { IntervalsModel, DayModel } from '../reducers/calendar/calendar.model';
 import { AppState } from '../reducers/app.reducer';
 import { CalendarEventsType } from '../reducers/calendar/calendar-events.model';
 import { CalendarIntervalColor, legendStyles as styles } from './styles';
@@ -9,27 +9,25 @@ import { StyledText } from '../override/styled-text';
 
 interface CalendarLegendProps {
     intervals: IntervalsModel;
-    weeks: WeekModel[];
+    selectedDay: DayModel;
 }
 
 const mapStateToProps = (state: AppState): CalendarLegendProps => ({
     intervals: state.calendar.calendarEvents.intervals,
-    weeks: state.calendar.calendarEvents.weeks
+    selectedDay: state.calendar.calendarEvents.selectedCalendarDay
 });
 
 class CalendarLegendImpl extends Component<CalendarLegendProps> {
     public render() {
-        const existingIntervals = this.getVisibleCalendarEvents();
-        const legend: JSX.Element[] = [];
-
-        existingIntervals.forEach((value, type) => {
+        const selectedEvents = this.getEventsForSelectedDate();
+        const legend = selectedEvents.map(type => {
             const style = StyleSheet.flatten([styles.marker, { backgroundColor: CalendarIntervalColor.getColor(type) }]);
-            legend.push((
+            return (
                 <View key={type} style={styles.itemContainer}>
                     <View style={style}></View>
                     <StyledText style={styles.label}>{type}</StyledText>
                 </View>
-            ));
+            );
         });
 
         return (
@@ -41,25 +39,14 @@ class CalendarLegendImpl extends Component<CalendarLegendProps> {
         );
     }
 
-    private getVisibleCalendarEvents() {
-        const { intervals, weeks } = this.props;
-        const calendarEventsMap: Map<CalendarEventsType, number> = new Map();
+    private getEventsForSelectedDate() {
+        const { intervals, selectedDay } = this.props;
 
-        if (!weeks || !intervals) {
-            return calendarEventsMap;
+        if (!selectedDay || !intervals) {
+            return [];
         }
 
-        weeks.forEach(week => {
-            week.days.forEach(day => {
-                const dayIntervals = intervals.get(day.date) || [];
-                dayIntervals.forEach(interval => {
-                    let eventCounter = calendarEventsMap.get(interval.eventType) || 0;
-                    calendarEventsMap.set(interval.eventType, ++eventCounter);
-                });
-            });
-        });
-
-        return calendarEventsMap;
+        return (intervals.get(selectedDay.date) || []).map(interval => interval.eventType);
     }
 }
 
