40bab05423f8f4bee3d0e76a32c67bde3082a1d2
Dementiy Kalkov <dementiy.kalkov@arcadia.spb.ru>
2018-02-22 12:51:36 +0300

    basic dialog structure

--- mobile/src/calendar/event-dialog/event-dialog.tsx
+++ mobile/src/calendar/event-dialog/event-dialog.tsx
@@ -2,14 +2,39 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { AppState } from '../../reducers/app.reducer';
 import { connect } from 'react-redux';
+import { ApplicationIcon } from '../../override/application-icon';
+import { StyledText } from '../../override/styled-text';
 
 interface EventDialogProps {
 }
 
 export class EventDialogImpl extends Component<EventDialogProps> {
     public render() {
+        const iconName = '';
+        const iconSize = 0;
+
+        const dialogTitle = '';
+        const dialogText = '';
+
         return (
             <View>
+                <View>
+                    <ApplicationIcon name={iconName} size={iconSize} />
+                </View>
+                <View>
+                    <StyledText>{dialogTitle}</StyledText>
+                    <View>
+                        <StyledText>{dialogText}</StyledText>
+                    </View>
+
+                    <View>
+                        {/* Button 1 */}
+                        {/* Button 2 */}
+                    </View>
+                </View>
+                <View>
+                    {/* Close dialog button */}
+                </View>
             </View>
         );
     }
