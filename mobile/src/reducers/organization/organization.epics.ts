f22cf8149450952e2d421f51a01dd8dcc68421e6
Victor Kotov <victor.kotov@arcadia.spb.ru>
2018-02-26 14:31:55 +0300

    tabs fix

--- mobile/src/reducers/organization/organization.epics.ts
+++ mobile/src/reducers/organization/organization.epics.ts
@@ -2,7 +2,8 @@ import { ActionsObservable, ofType } from 'redux-observable';
 import {
     LoadDepartments, loadDepartmentsFinished, LoadDepartmentsFinished, loadDepartments,
     loadEmployeeFinished, LoadEmployeesForDepartment, LoadEmployeesForRoom, loadEmployeesForDepartment,
-    LoadEmployee, loadEmployee, LoadEmployeeFinished, loadEmployeesForRoom } from './organization.action';
+    LoadEmployee, loadEmployee, LoadEmployeeFinished, loadEmployeesForRoom
+} from './organization.action';
 import { LoadUserEmployeeFinished } from '../user/user.action';
 import { deserializeArray, deserialize } from 'santee-dcts/src/deserializer';
 import { Department } from './department.model';
@@ -30,7 +31,7 @@ export const loadDepartmentsEpic$ = (action$: ActionsObservable<LoadDepartments>
         .map(x => loadDepartmentsFinished(x))
         .catch((e: Error) => Observable.of(loadFailedError(e.message)));
 
-export const loadChiefsEpic$ = (action$: ActionsObservable<LoadDepartmentsFinished> ) =>
+export const loadChiefsEpic$ = (action$: ActionsObservable<LoadDepartmentsFinished>) =>
     action$.ofType('LOAD-DEPARTMENTS-FINISHED')
         .flatMap(x => x.departments.map(dep => loadEmployee(dep.chiefId)));
 
@@ -45,20 +46,20 @@ export const loadEmployeesForDepartmentEpic$ = (action$: ActionsObservable<LoadE
         .catch((e: Error) => Observable.of(loadFailedError(e.message)));
 
 export const loadEmployeesForRoomEpic$ = (action$: ActionsObservable<LoadEmployeesForRoom>, state: AppState) =>
-        action$.ofType('LOAD_EMPLOYEES_FOR_ROOM')
-            .groupBy(x => x.roomNumber)
-            .map(x =>
-                x.switchMap(y =>
-                    ajaxGetJSON(`${url}/employees?roomNumber=${x.key}`).map(obj => deserializeArray(obj as any, Employee))))
-            .mergeAll()
-            .flatMap(x => x.map(loadEmployeeFinished))
-            .catch((e: Error) => Observable.of(loadFailedError(e.message)));
+    action$.ofType('LOAD_EMPLOYEES_FOR_ROOM')
+        .groupBy(x => x.roomNumber)
+        .map(x =>
+            x.switchMap(y =>
+                ajaxGetJSON(`${url}/employees?roomNumber=${x.key}`).map(obj => deserializeArray(obj as any, Employee))))
+        .mergeAll()
+        .flatMap(x => x.map(loadEmployeeFinished))
+        .catch((e: Error) => Observable.of(loadFailedError(e.message)));
 
 export const loadEmployeesForUserDepartmentEpic$ = (action$: ActionsObservable<LoadUserEmployeeFinished>) =>
-            action$.ofType('LOAD-USER-EMPLOYEE-FINISHED')
-                .map(x => loadEmployeesForDepartment(x.employee.departmentId));
-        
-        
+    action$.ofType('LOAD-USER-EMPLOYEE-FINISHED')
+        .map(x => loadEmployeesForDepartment(x.employee.departmentId));
+
+
 export const loadEmployeesForUserRoomEpic$ = (action$: ActionsObservable<LoadUserEmployeeFinished>) =>
-                action$.ofType('LOAD-USER-EMPLOYEE-FINISHED')
-                    .map(x => loadEmployeesForRoom(x.employee.roomNumber));
+    action$.ofType('LOAD-USER-EMPLOYEE-FINISHED')
+        .map(x => loadEmployeesForRoom(x.employee.roomNumber));
