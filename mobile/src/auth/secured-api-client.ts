ef3a8c97da24036a404e34b8a62e2aa6dc434cce
Shevnin, Alexander <alexander.shevnin@arcadia.spb.ru>
2018-03-13 16:29:47 +0300

    Added Secured api client

---
+++ mobile/src/auth/secured-api-client.ts
@@ -0,0 +1,42 @@
+import { ajaxGetJSON, ajaxPost, ajaxPut, ajaxDelete } from 'rxjs/observable/dom/AjaxObservable';
+import { Observable } from 'rxjs/Observable';
+import { AuthenticatedState } from './authentication-state';
+
+export class SecuredApiClient {
+
+    constructor(private apiRootUrl: string, private authState: Observable<AuthenticatedState>) {
+    }
+
+    public getJSON(relativeUrl: string, headers?: Object) {
+        return this.getHeaders(headers)
+            .flatMap(newHeaders => ajaxGetJSON(this.getFullUrl(relativeUrl), newHeaders));
+    }
+
+    public post(relativeUrl: string, body?: any, headers?: Object) {
+        return this.getHeaders(headers)
+            .flatMap(newHeaders => ajaxPost(this.getFullUrl(relativeUrl), body, newHeaders));
+    }
+
+    public put(relativeUrl: string, body?: any, headers?: Object) {
+        return this.getHeaders(headers)
+            .flatMap(newHeaders => ajaxPut(this.getFullUrl(relativeUrl), body, newHeaders));
+    }
+
+    public delete(relativeUrl: string, headers?: Object) {
+        return this.getHeaders(headers)
+            .flatMap(newHeaders => ajaxDelete(this.getFullUrl(relativeUrl), newHeaders));
+    }
+
+    private getFullUrl(relativeUrl: string) {
+        return new URL(relativeUrl, this.apiRootUrl).toString();
+    }
+
+    private getHeaders(headers?: Object) {
+        return this.authState
+            .first(x => x.isAuthenticated)
+            .map(x => ({
+                ...headers,
+                'Authorization': `Bearer ${x.jwtToken}`
+            }));
+    }
+}
