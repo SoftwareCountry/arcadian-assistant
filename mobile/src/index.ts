a884dd6212bd37b61ce6d0e29b3ab81566d04ab5
Shevnin, Alexander <alexander.shevnin@arcadia.spb.ru>
2018-03-15 18:49:04 +0300

    Added polyfill for urlsearchparams

--- mobile/src/index.ts
+++ mobile/src/index.ts
@@ -1,6 +1,8 @@
 import { AppRegistry, Alert } from 'react-native';
 import { Root } from './root';
 
+import 'url-search-params-polyfill';
+
 const defaultHandler = ErrorUtils.getGlobalHandler && ErrorUtils.getGlobalHandler();
 ErrorUtils.setGlobalHandler(function(error: any, isFatal: true) {
     console.error('Unhandled error ocurred');
