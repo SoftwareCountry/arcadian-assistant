16921d4bd0f2ca8c0489a08c0869f50b3caf92d8
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2018-05-07 20:28:16 +0300

    Well known paths added

---
+++ server/Arcadia.Assistant.Server.Interop/WellKnownActorPaths.cs
@@ -0,0 +1,11 @@
+﻿namespace Arcadia.Assistant.Server.Interop
+{
+    public static class WellKnownActorPaths
+    {
+        public static readonly string Organization = "organization";
+
+        public static readonly string Helpdesk = "helpdesk";
+
+        public static readonly string SharedFeeds = "shared-feeds";
+    }
+}
