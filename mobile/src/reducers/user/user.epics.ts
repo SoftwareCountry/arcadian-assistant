7a8c25fae8ddbc9de945bcd3d743067cdd914195
Elena Tsvetkova <elena.tsvetkova@arcadia.spb.ru>
2018-05-28 15:54:56 +0300

    Handle http errors and logout if no user employee.

--- mobile/src/reducers/user/user.epics.ts
+++ mobile/src/reducers/user/user.epics.ts
@@ -9,19 +9,22 @@ import { AppState } from 'react-native';
 import { DependenciesContainer } from '../app.reducer';
 import { Employee } from '../organization/employee.model';
 import { handleHttpErrors } from '../errors/errors.epics';
+import { startLogoutProcess } from '../auth/auth.action';
 
 // TODO: Handle error, display some big alert blocking app...
 export const loadUserEpic$ = (action$: ActionsObservable<LoadUser>, appState: AppState, deps: DependenciesContainer) =>
     action$.ofType('LOAD-USER')
         .switchMap(x => deps.apiClient.getJSON(`/user`)
-            .pipe(handleHttpErrors()))
+            .pipe(handleHttpErrors(false)))
         .map(x => deserialize(x, User))
-        .map(x => loadUserFinished(x.employeeId));
+        .map(x => loadUserFinished(x.employeeId))
+        .catch(e => Observable.of(startLogoutProcess()));
 
 export const loadUserFinishedEpic$ = (action$: ActionsObservable<LoadUserFinished>, appState: AppState, deps: DependenciesContainer) =>
     action$.ofType('LOAD-USER-FINISHED')
         .switchMap(x => deps.apiClient.getJSON(`/employees/${x.userEmployeeId}`)
-            .pipe(handleHttpErrors()))
+            .pipe(handleHttpErrors(false)))
         .map(obj => deserialize(obj, Employee))
-        .map(z => loadUserEmployeeFinished(z));
+        .map(z => loadUserEmployeeFinished(z))
+        .catch(e => Observable.of(startLogoutProcess()));
 
