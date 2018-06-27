40134a04e0f0a14427f1eb3f895acc67f80dd87d
sergey.kim <sergey.kim@arcadia.spb.ru>
2018-06-27 11:48:26 +0300

    refactoring extract permission names

--- server/Arcadia.Assistant.Web/Models/UserEmployeePermissionsModel.cs
+++ server/Arcadia.Assistant.Web/Models/UserEmployeePermissionsModel.cs
@@ -22,11 +22,10 @@
         {
             return employeePermissionsEntry
                 .ToString("G")
-                .Split(',')
+                .Split(", ")
                 .Select(x =>
                 {
-                    var trimmed = x.Trim();
-                    var camelCased = char.ToLowerInvariant(trimmed[0]) + trimmed.Substring(1);
+                    var camelCased = char.ToLowerInvariant(x[0]) + x.Substring(1);
                     return camelCased;
                 })
                 .ToArray();
