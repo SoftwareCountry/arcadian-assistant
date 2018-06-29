73c812fcf30b6510bac466fe15aca26f90bdb50e
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2018-06-29 15:20:38 +0300

    Continue fixes for db recovery issue

--- server/Arcadia.Assistant.Calendar/SickLeave/SendEmailSickLeaveActor.cs
+++ server/Arcadia.Assistant.Calendar/SickLeave/SendEmailSickLeaveActor.cs
@@ -20,7 +20,7 @@
         private readonly IEmailSettings mailConfig;
         private readonly ISmtpSettings smtpConfig;
         private readonly ILoggingAdapter logger = Context.GetLogger();
-        private IActorRef employeesActor = null;
+        private IActorRef employeesActor;
 
         public SendEmailSickLeaveActor(IEmailSettings mailConfig, ISmtpSettings smtpConfig)
         {
@@ -38,20 +38,26 @@
             switch (message)
             {
                 case SickLeaveEmail email:
-                    SendEmail(email);
+                    this.SendEmail(email);
                     break;
+
                 case SickLeaveIsApproved ev:
-                    if (employeesActor != null)
-                        employeesActor.Ask(new EmployeesQuery().WithId(ev.UserId))
-                            .ContinueWith(result => new SickLeaveEmail(ev.TimeStamp, result.Result.AsInstanceOf<EmployeesQuery.Response>()),
-                                TaskContinuationOptions.AttachedToParent & TaskContinuationOptions.ExecuteSynchronously)
-                            .PipeTo(Self);
+                    if (this.employeesActor != null)
+                    {
+                        this.employeesActor
+                            .Ask(new EmployeesQuery().WithId(ev.UserId))
+                            .ContinueWith(result => new SickLeaveEmail(ev.TimeStamp, result.Result.AsInstanceOf<EmployeesQuery.Response>()), TaskContinuationOptions.AttachedToParent & TaskContinuationOptions.ExecuteSynchronously)
+                            .PipeTo(this.Self);
+                    }
                     else
-                        logger.Debug("Employees actor ref is not set in sending email actor");
+                    {
+                        this.logger.Warning($"Employees actor ref is not set in sending email actor. {Context.Self}");
+                    }
 
                     break;
+
                 case SetEmployeesActor actor:
-                    employeesActor = actor.employeesActor;
+                    this.employeesActor = actor.EmployeesActor;
                     break;
             }
         }
@@ -60,24 +66,27 @@
         {
             using (var client = new SmtpClient())
             {
-                logger.Debug("Sending a sick leave email notification for user {0}", email.GetEmployeeId());
-                var msg = CreateMimeMessage(email.GetBody(mailConfig.Body));
+                this.logger.Debug("Sending a sick leave email notification for user {0}", email.GetEmployeeId());
+                var msg = this.CreateMimeMessage(email.GetBody(this.mailConfig.Body));
 
-                client.Connect(smtpConfig.Host, smtpConfig.Port, smtpConfig.UseTls ? SecureSocketOptions.StartTls 
+                client.Connect(
+                    this.smtpConfig.Host,
+                    this.smtpConfig.Port,
+                    this.smtpConfig.UseTls ? SecureSocketOptions.StartTls 
                                                                         : SecureSocketOptions.SslOnConnect);
-                client.Authenticate(smtpConfig.User, smtpConfig.Password);
+                client.Authenticate(this.smtpConfig.User, this.smtpConfig.Password);
                 client.Send(msg);
                 client.Disconnect(true);
-                logger.Debug("Sick leave email notification for user {0} was succesfully sent", email.GetEmployeeId());
+                this.logger.Debug("Sick leave email notification for user {0} was succesfully sent", email.GetEmployeeId());
             }
         }
 
-        public MimeMessage CreateMimeMessage(string body)
+        private MimeMessage CreateMimeMessage(string body)
         {
             var message = new MimeMessage();
-            message.From.Add(new MailboxAddress("From", mailConfig.NotificationSender));
-            message.To.Add(new MailboxAddress("To", mailConfig.NotificationRecipient));
-            message.Subject = mailConfig.Subject;
+            message.From.Add(new MailboxAddress("From", this.mailConfig.NotificationSender));
+            message.To.Add(new MailboxAddress("To", this.mailConfig.NotificationRecipient));
+            message.Subject = this.mailConfig.Subject;
             message.Body = new TextPart("plain") { Text = body };
 
             return message;
@@ -87,32 +96,33 @@
         {
             public SickLeaveEmail(DateTimeOffset timeStamp, EmployeesQuery.Response response)
             {
-                this.timeStamp = timeStamp;
-                employee = response.Employees.FirstOrDefault();
+                this.TimeStamp = timeStamp;
+                this.Employee = response.Employees.FirstOrDefault();
             }
 
             public string GetBody(string format)
             {
-                return string.Format(format, employee.Metadata.Name, timeStamp.ToString());
+                return string.Format(format, this.Employee.Metadata.Name, this.TimeStamp.ToString());
             }
 
             public string GetEmployeeId()
             {
-                return employee.Metadata.EmployeeId;
+                return this.Employee.Metadata.EmployeeId;
             }
 
-            private DateTimeOffset timeStamp { get; }
-            private EmployeeContainer employee { get; }
+            private DateTimeOffset TimeStamp { get; }
+
+            private EmployeeContainer Employee { get; }
         }
 
         public sealed class SetEmployeesActor
         {
             public SetEmployeesActor(IActorRef emplActor)
             {
-                employeesActor = emplActor;
+                this.EmployeesActor = emplActor;
             }
 
-            public IActorRef employeesActor { get; }
+            public IActorRef EmployeesActor { get; }
         }
     }
 }
