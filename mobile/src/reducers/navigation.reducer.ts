20fb8f05da8f0e015ebc83592ea1bd5ef6eb5326
Natalia Ponomareva <natalia.ponomareva@arcadia.spb.ru>
2018-07-27 17:13:48 +0300

    Fixed bug with double clicking on photo

--- mobile/src/reducers/navigation.reducer.ts
+++ mobile/src/reducers/navigation.reducer.ts
@@ -1,10 +1,22 @@
 import { RootNavigator } from '../tabbar/tab-navigator';
-import { NavigationAction, NavigationState } from 'react-navigation';
+import { NavigationAction, NavigationState, NavigationActions } from 'react-navigation';
 import { PeopleScreenNavigator } from '../people/navigator/people-screen-navigator';
 
 export const navigationReducer = (state: NavigationState, action: NavigationAction) => {
-    const nextState = RootNavigator.router.getStateForAction(action, state);
-    return nextState || state;
+    const nextState: NavigationState = RootNavigator.router.getStateForAction(action, state);
+    switch (action.type) {
+        case NavigationActions.NAVIGATE:
+            const toNavigate: NavigationState = nextState.routes.find(e => e.isTransitioning);
+            if (toNavigate) {
+                const length = toNavigate.routes.length;
+                const lastRoute = length >= 2 ? toNavigate.routes[length - 2].routeName : undefined;
+                if (state && lastRoute && action.routeName === lastRoute) {
+                    return state;
+                }
+            }
+        default:
+            return nextState;
+    }
 };
 
 export const navigationMiddlewareKeyReducer = (state: string = '') => {
