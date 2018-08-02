a6dab9712279f27ebb1b43c68f641a3f55b1ec2e
Kim <sergey.kim@sdo822covmac1.arcadia.intranet>
2018-08-02 15:59:11 +0300

    Fix seach text input Use display style instead of variable to hide the text input

--- mobile/src/navigation/search-view-styles.tsx
+++ mobile/src/navigation/search-view-styles.tsx
@@ -4,8 +4,8 @@ export const searchViewStyles = StyleSheet.create({
     container: {
         paddingLeft: 5,
         paddingRight: 5,
-        paddingTop: Platform.OS === 'ios' ? 40 : 0,
-        height: Platform.OS === 'ios' ? 60 : 40,
+        paddingTop: Platform.OS === 'ios' ? 45 : 0,
+        height: Platform.OS === 'ios' ? 65 : 40,
         backgroundColor: '#2FAFCC',
         flexDirection: 'row',
         alignItems: 'center'
