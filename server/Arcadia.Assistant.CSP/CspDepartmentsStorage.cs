22121cbf660460025634735c4d58210d6a18913d
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2018-10-13 12:56:01 +0300

    Filtering empty department branches

--- server/Arcadia.Assistant.CSP/CspDepartmentsStorage.cs
+++ server/Arcadia.Assistant.CSP/CspDepartmentsStorage.cs
@@ -24,14 +24,22 @@
 
         private const string PriorityHeadDepartment = "GMG";
 
-        private readonly Expression<Func<Department, DepartmentInfo>> mapDepartment =
-            x =>
+        private readonly Expression<Func<Department, IEnumerable<Employee>, DepartmentInfo>> mapDepartment =
+            (x, chiefsGroup) =>
                 new DepartmentInfo(
                     x.Id.ToString(),
                     x.Name,
                     x.Abbreviation,
                     (x.ParentDepartmentId == null) || (x.Id == x.ParentDepartmentId) ? null : x.ParentDepartmentId.ToString())
-                    { ChiefId = x.ChiefId == null ? null : x.ChiefId.ToString() };
+                { ChiefId = (x.ChiefId == null) || !chiefsGroup.Any() ? null : x.ChiefId.ToString() };
+
+        private readonly Expression<Func<DepartmentInfo, IEnumerable<Employee>, DepartmentInfoWithPeopleCount>> countEmployees =
+            (d, employees) =>
+                new DepartmentInfoWithPeopleCount()
+                {
+                    Department = d,
+                    PeopleCount = employees.Count()
+                };
 
         protected override async Task<LoadAllDepartments.Response> GetAllDepartments()
         {
@@ -42,46 +50,53 @@
                 var allDepartments = await context
                     .Department
                     .Where(x => (x.IsDelete != true) && (x.CompanyId == ArcadiaCompanyId))
-                    .Join(arcEmployees, o => o.ChiefId, i => i.Id, (d, e) => d)
-                    .Select(this.mapDepartment)
+                    .GroupJoin(arcEmployees, d => d.ChiefId, e => e.Id, this.mapDepartment)
+                    .GroupJoin(arcEmployees, d => d.DepartmentId, e => e.DepartmentId.ToString(), this.countEmployees)
                     .ToListAsync();
 
-                var head = allDepartments.FirstOrDefault(x => x.IsHeadDepartment && (x.Abbreviation == PriorityHeadDepartment));
+                var head = allDepartments.FirstOrDefault(x => x.Department.IsHeadDepartment && (x.Department.Abbreviation == PriorityHeadDepartment));
 
-                var filteredDepartments = new List<DepartmentInfo>();
-
-                if (head != null)
+                if (head == null)
                 {
-                    filteredDepartments.Add(head);
-                    filteredDepartments.AddRange(GetDescendants(allDepartments, head, new HashSet<string>()));
+                    return new LoadAllDepartments.Response(new DepartmentInfo[0]);
                 }
 
-                return new LoadAllDepartments.Response(filteredDepartments);
+                var tree = new DepartmentsTreeNode(head.Department, head.PeopleCount);
+                tree.Children.AddRange(this.CreateTree(allDepartments, tree.DepartmentInfo.DepartmentId, new HashSet<string>() { head.Department.DepartmentId }));
+
+                var departments = tree.AsEnumerable().Where(x => x.CountAllEmployees() != 0).Select(x => x.DepartmentInfo).ToList();
+
+                return new LoadAllDepartments.Response(departments);
             }
         }
 
-        /// <param name="node">Node for which descandants are requested</param>
+        private class DepartmentInfoWithPeopleCount
+        {
+            public DepartmentInfo Department { get; set; }
+
+            public int PeopleCount { get; set; }
+        }
+
+        /// <param name="departmentId">Node for which descandants are requested</param>
         /// <param name="processedIds">a hashset with processed department ids to prevent stackoverflow</param>
         /// <param name="allDepartments">all possible departments</param>
-        private static List<DepartmentInfo> GetDescendants(
-            IReadOnlyCollection<DepartmentInfo> allDepartments,
-            DepartmentInfo node,
+        private List<DepartmentsTreeNode> CreateTree(IReadOnlyCollection<DepartmentInfoWithPeopleCount> allDepartments,
+            string departmentId,
             HashSet<string> processedIds)
         {
             var children = allDepartments
-                .Where(x => (x.ParentDepartmentId == node.DepartmentId) && !processedIds.Contains(x.DepartmentId))
+                .Where(x => (x.Department.ParentDepartmentId == departmentId) && !processedIds.Contains(x.Department.DepartmentId))
+                .Select(x => new DepartmentsTreeNode(x.Department, x.PeopleCount))
                 .ToList();
 
-            processedIds.UnionWith(children.Select(x => x.DepartmentId));
-
-            var descendants = new List<DepartmentInfo>(children);
+            processedIds.UnionWith(children.Select(x => x.DepartmentInfo.DepartmentId));
 
             foreach (var child in children)
             {
-                descendants.AddRange(GetDescendants(allDepartments, child, processedIds));
+                child.Children.AddRange(this.CreateTree(allDepartments, child.DepartmentInfo.DepartmentId, processedIds));
             }
 
-            return descendants;
+            return children;
         }
     }
 }
---
+++ server/Arcadia.Assistant.CSP/DepartmentsTreeNode.cs
@@ -0,0 +1,40 @@
+﻿namespace Arcadia.Assistant.CSP
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Arcadia.Assistant.Organization.Abstractions;
+
+    public class DepartmentsTreeNode
+    {
+        public int EmployeesCount { get; }
+
+        public DepartmentInfo DepartmentInfo { get; }
+
+        public DepartmentsTreeNode(DepartmentInfo departmentInfo, int employeesCount)
+        {
+            this.DepartmentInfo = departmentInfo;
+            this.EmployeesCount = employeesCount;
+        }
+
+        public List<DepartmentsTreeNode> Children { get; } = new List<DepartmentsTreeNode>();
+
+        public int CountAllEmployees()
+        {
+            return this.Children.Sum(x => x.CountAllEmployees()) + this.EmployeesCount;
+        }
+
+        public IEnumerable<DepartmentsTreeNode> AsEnumerable()
+        {
+            yield return this;
+
+            foreach (var child in this.Children)
+            {
+                foreach (var d in child.AsEnumerable())
+                {
+                    yield return d;
+                }
+            }
+        }
+    }
+}
