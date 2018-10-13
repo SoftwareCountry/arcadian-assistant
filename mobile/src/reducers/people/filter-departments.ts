0601967c0f7d94e0507ace1e8f9623e343014757
sergey.kim <sergey.kim@arcadia.spb.ru>
2018-10-13 22:25:17 +0300

    Optimization

--- mobile/src/reducers/people/filter-departments.ts
+++ mobile/src/reducers/people/filter-departments.ts
@@ -6,10 +6,9 @@ export function filterDepartments(
     departments: Department[],
     employeeIdToNode: EmployeeIdToNode
 ): Department[] {
-
+    const employeeNodes = employeeIdToNode.toArray();
+    
     const filteredDepartments = departments.filter(department => {
-
-        const employeeNodes = employeeIdToNode.toArray();
 
         for (let employeeNode of employeeNodes) {
             if (employeeNode.get('departmentId') === department.departmentId) {
