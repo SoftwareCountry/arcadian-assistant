3b7f235861aaddcc82294ebe97ae2cb48419aa4e
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2018-11-14 19:40:40 +0300

    Added empty health controller and model.

---
+++ server/Arcadia.Assistant.Web/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿namespace Arcadia.Assistant.Web.Controllers
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+
+    public class HealthController : Controller
+    {
+        [Route("/api/ping/health")]
+        [HttpGet]
+        [ProducesResponseType(typeof(ApplicationHealthModel), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetApplicationHealth(CancellationToken cancellationToken)
+        {
+            return this.Ok(new ApplicationHealthModel());
+        }
+    }
+}
---
+++ server/Arcadia.Assistant.Web/Models/ApplicationHealthModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Arcadia.Assistant.Web.Models
+{
+    public class ApplicationHealthModel
+    {
+        public bool IsServerAlive { get; set; }
+
+        public bool Is1CAlive { get; set; }
+
+        public bool IsDatabaseAlive { get; set; }
+    }
+}
