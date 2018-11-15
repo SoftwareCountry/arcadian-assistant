94c6ba0b8aae294e13d04685d3c2335e3a66a3a5
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2018-11-16 01:29:29 +0300

    Added real checks for database and 1C accessibility.

--- server/Arcadia.Assistant.Health/HealthActor.cs
+++ server/Arcadia.Assistant.Health/HealthActor.cs
@@ -1,10 +1,28 @@
 ﻿namespace Arcadia.Assistant.Health
 {
+    using System;
+    using System.Data;
+    using System.Net;
+    using System.Threading.Tasks;
+
     using Abstractions;
     using Akka.Actor;
+    using CSP;
+    using CSP.Model;
+    using CSP.Vacations;
+    using Microsoft.EntityFrameworkCore;
 
     public class HealthActor : UntypedActor, ILogReceive
     {
+        private readonly Func<ArcadiaCspContext> contextFactory;
+        private readonly Remote1CConfiguration bkConfiguration;
+
+        public HealthActor(Func<ArcadiaCspContext> contextFactory, Remote1CConfiguration bkConfiguration)
+        {
+            this.contextFactory = contextFactory;
+            this.bkConfiguration = bkConfiguration;
+        }
+
         protected override void OnReceive(object message)
         {
             if (!(message is HealthCheckMessage healthMessage))
@@ -20,16 +38,64 @@
                     break;
 
                 case HealthCheckType.Check1C:
-                    this.Sender.Tell(true);
+                    this.Check1C().PipeTo(this.Sender);
                     break;
 
                 case HealthCheckType.CheckDatabase:
-                    this.Sender.Tell(true);
+                    this.Sender.Tell(this.CheckDatabase());
                     break;
 
                 default:
                     this.Unhandled(message);
                     break;
+            }
+        }
+
+        private async Task<bool> Check1C()
+        {
+            try
+            {
+                var uriBuilder = new UriBuilder(this.bkConfiguration.Url);
+
+                var client = new EnterpriseV8(uriBuilder.Uri)
+                {
+                    Credentials = new NetworkCredential
+                    {
+                        UserName = this.bkConfiguration.Username,
+                        Password = this.bkConfiguration.Password
+                    }
+                };
+
+                var taskFactory = new TaskFactory();
+
+                await taskFactory.FromAsync(
+                    client.Catalog_ВидыОтпусков.BeginExecute,
+                    client.Catalog_ВидыОтпусков.EndExecute,
+                    new object());
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private bool CheckDatabase()
+        {
+            try
+            {
+                using (var context = this.contextFactory())
+                {
+                    var connection = context.Database.GetDbConnection();
+                    connection.Open();
+
+                    return connection.State == ConnectionState.Open;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
             }
         }
     }
