6c7dd2da78bae592c5353437dc93cb3a3b1a1d10
Anton Myshkovskiy <anton.myshkovskiy@arcadia.spb.ru>
2018-11-19 19:06:46 +0300

    On Android feeds do not load next page when list is scrolled to the bottom #249
    1. onEndReachedThreshold has to be >0 to onEndReached to be fired
    2. Footer with activity indicator is added

--- mobile/src/navigation/loading.tsx
+++ mobile/src/navigation/loading.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { View, ActivityIndicator } from 'react-native';
 import { searchViewStyles as styles } from './search-view-styles';
 
+//============================================================================
 export class LoadingView extends React.Component {
     public render() {
         return (
             <View style={styles.loadingContainer}>
-                <ActivityIndicator></ActivityIndicator>
+                <ActivityIndicator size={'large'} color={'#2FAFCC'}/>
             </View>
         );
     }
