cb2df109e8e0caf4ef5d2316853452313d95608d
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2018-11-21 15:45:52 +0300

    Added basic authentication for health endpoint. #231

---
+++ server/Arcadia.Assistant.Web/Authorization/Handlers/UserIsHealthHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace Arcadia.Assistant.Web.Authorization.Handlers
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Authorization;
+    using Requirements;
+
+    public class UserIsHealthHandler : AuthorizationHandler<UserIsHealthRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, UserIsHealthRequirement requirement)
+        {
+            context.Succeed(requirement);
+            return Task.CompletedTask;
+        }
+    }
+}
