137574ae14ff1df02da24197377783425514b30a
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2018-11-21 12:28:31 +0300

    Returned 503 instead of 200 if any health state check is failed. #231

--- server/Arcadia.Assistant.Web/Controllers/HealthController.cs
+++ server/Arcadia.Assistant.Web/Controllers/HealthController.cs
@@ -22,6 +22,7 @@
         [Route("/api/ping/health")]
         [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<ApplicationHealthModelEntry>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(IEnumerable<ApplicationHealthModelEntry>), StatusCodes.Status503ServiceUnavailable)]
         public async Task<IActionResult> GetApplicationHealth(CancellationToken cancellationToken)
         {
             var healthStateDict = await this.healthService.GetHealthState(cancellationToken);
@@ -29,6 +30,11 @@
             var result = healthStateDict
                 .Select(kvp => new ApplicationHealthModelEntry(kvp.Key, kvp.Value.Value, kvp.Value.Details))
                 .ToList();
+
+            if (result.Any(x => !x.StateValue))
+            {
+                return this.StatusCode(StatusCodes.Status503ServiceUnavailable, result);
+            }
 
             return this.Ok(result);
         }
