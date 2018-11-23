46d835c705718143107abe450da4050541e4bc0d
Artem Kirienko <artem.kirienko@gmail.com>
2018-11-23 16:23:41 +0300

    #4: ReactNativeConfig dependency added

--- mobile/android/app/src/main/java/ru/spb/arcadia/product/assistant/android/MainApplication.java
+++ mobile/android/app/src/main/java/ru/spb/arcadia/product/assistant/android/MainApplication.java
@@ -3,6 +3,7 @@ package ru.spb.arcadia.product.assistant.android;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.hieuvp.fingerprint.ReactNativeFingerprintScannerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import br.com.classapp.RNSensitiveInfo.RNSensitiveInfoPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeConfigPackage(),
             new ReactNativeFingerprintScannerPackage(),
             new VectorIconsPackage(),
             new RNSensitiveInfoPackage(),
