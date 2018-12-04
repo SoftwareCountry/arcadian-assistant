b726952746b38f4b6becd42b1f8e21e20a3d886b
Anton Myshkovskiy <anton.myshkovskiy@arcadia.spb.ru>
2018-12-04 16:57:28 +0300

    Formatting

--- mobile/src/reducers/auth/auth.epics.ts
+++ mobile/src/reducers/auth/auth.epics.ts
@@ -1,20 +1,22 @@
-import { ActionsObservable, ofType, combineEpics, StateObservable } from 'redux-observable';
-import { DependenciesContainer, AppState } from '../app.reducer';
-import { StartLoginProcess, StartLogoutProcess, startLoginProcess, startLogoutProcess, userLoggedIn, userLoggedOut, jwtTokenSet } from '../auth/auth.action';
+import { ActionsObservable, StateObservable } from 'redux-observable';
+import { AppState, DependenciesContainer } from '../app.reducer';
+import { jwtTokenSet, StartLoginProcess, StartLogoutProcess, userLoggedIn, userLoggedOut } from './auth.action';
 import { refresh } from '../refresh/refresh.action';
 import { handleHttpErrors } from '../errors/errors.epics';
-import {flatMap, distinctUntilChanged, map, filter, ignoreElements, tap} from 'rxjs/operators';
+import { distinctUntilChanged, flatMap, ignoreElements, map, tap } from 'rxjs/operators';
 import { Alert } from 'react-native';
-import {AuthenticationState, AuthenticatedState, NotAuthenticatedState} from '../../auth/authentication-state';
+import { AuthenticationState } from '../../auth/authentication-state';
 import { Action } from 'redux';
 import { concat, of } from 'rxjs';
-
 
 function showAlert(message: string, okButtonTitle: string, rejectButtonTitle: string, okButton: () => void, rejectButton: () => void) {
     Alert.alert(
         'Confirmation',
         `${message}`,
-        [{ text: okButtonTitle, onPress: () => okButton() }, { text: rejectButtonTitle, onPress: () => rejectButton() }]);
+        [{ text: okButtonTitle, onPress: () => okButton() }, {
+            text: rejectButtonTitle,
+            onPress: () => rejectButton()
+        }]);
 }
 
 export const startLoginProcessEpic$ = (action$: ActionsObservable<StartLoginProcess>, _: StateObservable<AppState>, dep: DependenciesContainer) =>
@@ -35,7 +37,8 @@ export const startLogoutProcessEpic$ = (action$: ActionsObservable<StartLogoutPr
                 'Logout',
                 'Cancel',
                 () => dep.oauthProcess.logout(),
-                () => {});
+                () => {
+                });
         }),
         ignoreElements()
     );
