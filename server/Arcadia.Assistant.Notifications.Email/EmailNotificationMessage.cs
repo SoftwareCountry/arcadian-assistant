12c301abce5bec570381ca85790f5db8d2c82773
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2018-12-06 01:41:45 +0300

    Added email notification actor.

---
+++ server/Arcadia.Assistant.Notifications.Email/EmailNotificationMessage.cs
@@ -0,0 +1,21 @@
+﻿namespace Arcadia.Assistant.Notifications.Email
+{
+    public class EmailNotificationMessage
+    {
+        public EmailNotificationMessage(string sender, string recipient, string subject, string body)
+        {
+            this.Sender = sender;
+            this.Recipient = recipient;
+            this.Subject = subject;
+            this.Body = body;
+        }
+
+        public string Sender { get; }
+
+        public string Recipient { get; }
+
+        public string Subject { get; }
+
+        public string Body { get; }
+    }
+}
