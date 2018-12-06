3c6f3c109befece3fdbe5bf7c04fa09fceb7374d
Anton Myshkovskiy <anton.myshkovskiy@arcadia.spb.ru>
2018-12-06 16:55:42 +0300

    loadPendingRequests and loadCalendarEvents upon profile refresh
    + styles

--- mobile/src/reducers/refresh/refresh.epics.ts
+++ mobile/src/reducers/refresh/refresh.epics.ts
@@ -1,13 +1,19 @@
-import { ActionsObservable, ofType } from 'redux-observable';
-import { DependenciesContainer, AppState } from '../app.reducer';
-import { loadFailedError } from '../errors/errors.action';
-import { loadUser } from '../user/user.action';
-import { loadDepartments } from '../organization/organization.action';
+import { ActionsObservable, ofType, StateObservable } from 'redux-observable';
+import { loadUser, LoadUserEmployeeFinished } from '../user/user.action';
 import { Refresh } from './refresh.action';
-import { flatMap } from 'rxjs/operators';
+import { filter, flatMap } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { loadPendingRequests } from '../calendar/pending-requests/pending-requests.action';
+import { loadCalendarEvents } from '../calendar/calendar.action';
+import { AppState } from '../app.reducer';
 
 export const refreshEpic$ = (action$: ActionsObservable<Refresh>) =>
     action$.ofType('REFRESH').pipe(
         flatMap(x => of(loadUser())),
     );
+
+export const refreshUserProfileData$ = (action$: ActionsObservable<LoadUserEmployeeFinished>, state$: StateObservable<AppState>) => action$.pipe(
+    ofType('LOAD-USER-EMPLOYEE-FINISHED'),
+    filter(() => !!state$.value.userInfo && !!state$.value.userInfo.employeeId),
+    flatMap(() => of(loadPendingRequests(), loadCalendarEvents(state$.value.userInfo!.employeeId!))),
+);
--- mobile/src/welcome-screen/styles.ts
+++ mobile/src/welcome-screen/styles.ts
@@ -3,19 +3,20 @@ import { StyleSheet } from 'react-native';
 export const welcomeScreenColor = '#2FAFCC';
 export const welcomeScreenStyles = StyleSheet.create({
     container: {
-        backgroundColor: welcomeScreenColor, 
-        flex: 1, 
-        justifyContent: 'center', 
+        backgroundColor: welcomeScreenColor,
+        flex: 1,
+        justifyContent: 'center',
         alignItems: 'center'
     },
     greeting: {
-        color: '#fff', 
-        fontSize: 26, 
-        paddingBottom: 100
-    },  
-    loginbuttonContainer: {
-        borderRadius: 50, 
-        backgroundColor: '#fff', 
-        width: 250 
+        color: '#fff',
+        fontSize: 26,
+        paddingBottom: 100,
+        textAlign: 'center',
+    },
+    loginButtonContainer: {
+        borderRadius: 50,
+        backgroundColor: '#fff',
+        width: 250
     }
 });
