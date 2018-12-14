00c0752c3bd5f65cb04c8c863d17b31d8c862825
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2018-12-14 20:01:37 +0300

    Removed Task.Wait usings. Refactored push notifications actor to using of asyn/await and PipeTo.

--- server/Arcadia.Assistant.Notifications.Push/PushNotificationsActor.cs
+++ server/Arcadia.Assistant.Notifications.Push/PushNotificationsActor.cs
@@ -1,11 +1,10 @@
 ﻿namespace Arcadia.Assistant.Notifications.Push
 {
-    using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Net;
     using System.Net.Http;
     using System.Net.Http.Headers;
-    using System.Threading;
     using System.Threading.Tasks;
 
     using Akka.Actor;
@@ -18,10 +17,9 @@
 
     public class PushNotificationsActor : UntypedActor, ILogReceive
     {
-        private readonly TimeSpan timeout = TimeSpan.FromSeconds(10);
-
         private readonly IPushSettings pushSettings;
         private readonly IHttpClientFactory httpClientFactory;
+
         public readonly ILoggingAdapter logger = Context.GetLogger();
 
         public PushNotificationsActor(IPushSettings pushSettings, IHttpClientFactory httpClientFactory)
@@ -35,49 +33,63 @@
             switch (message)
             {
                 case PushNotification msg when this.pushSettings.Enabled:
-                    this.SendPushNotification(msg);
+                    this.SendPushNotification(msg)
+                        .PipeTo(
+                            this.Self,
+                            success: x => new SendPushNotificationResult(x),
+                            failure: err => new SendPushNotificationResultFinish(err.Message));
+                    break;
+
+                case SendPushNotificationResult msg:
+                    this.HandleSendPushNotificationResults(msg.Responses)
+                        .PipeTo(
+                            this.Self,
+                            success: () => SendPushNotificationResultFinish.Instance,
+                            failure: err => new SendPushNotificationResultFinish(err.Message));
+                    break;
+
+                case SendPushNotificationResultFinish msg:
+                    if (msg.ErrorMessage != null)
+                    {
+                        this.logger.Warning(msg.ErrorMessage);
+                    }
+
                     break;
             }
         }
 
-        private void SendPushNotification(PushNotification message)
+        private Task<HttpResponseMessage[]> SendPushNotification(PushNotification message)
         {
             this.logger.Debug("Push notification message received");
 
             var jsonMessage = this.SerializeNotification(message);
             this.logger.Debug($"Serialized notification message: {jsonMessage}");
 
-            var cancellationSource = new CancellationTokenSource(this.timeout);
-
             var requests = this.pushSettings.ApplicationPushUrls
-                .Select(url => this.SendPushNotificationRequest(jsonMessage, url, cancellationSource.Token))
-                .ToArray();
+                .Select(url => this.SendPushNotificationRequest(url, jsonMessage));
 
-            Task.WaitAll(requests, CancellationToken.None);
+            return Task.WhenAll(requests);
+        }
 
-            var failedRequestsResults = requests
-                .Where(r => r.Result.StatusCode != HttpStatusCode.Accepted)
-                .Select(r => r.Result)
-                .ToList();
-
-            if (failedRequestsResults.Count == 0)
+        private async Task HandleSendPushNotificationResults(IEnumerable<HttpResponseMessage> responses)
+        {
+            foreach (var response in responses)
             {
-                this.logger.Debug("Push notification was succesfully sent");
-                return;
-            }
-
-            foreach (var result in failedRequestsResults)
-            {
-                var contentToStringTask = result.Content.ReadAsStringAsync();
-                contentToStringTask.Wait(CancellationToken.None);
-                this.logger.Warning($"Push notification to {result.RequestMessage.RequestUri} failed: {contentToStringTask.Result}");
+                if (response.StatusCode == HttpStatusCode.Accepted)
+                {
+                    this.logger.Debug($"Push notification was succesfully sent to {response.RequestMessage.RequestUri}");
+                }
+                else
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    this.logger.Warning($"Push notification to {response.RequestMessage.RequestUri} failed: {content}");
+                }
             }
         }
 
         private Task<HttpResponseMessage> SendPushNotificationRequest(
-            string pushNotificationContent,
             string applicationPushUrl,
-            CancellationToken cancellationToken)
+            string pushNotificationContent)
         {
             var content = new StringContent(pushNotificationContent);
             content.Headers.ContentType.MediaType = "application/json";
@@ -91,7 +103,7 @@
 
             using (var httpClient = this.httpClientFactory.CreateClient())
             {
-                return httpClient.SendAsync(request, cancellationToken);
+                return httpClient.SendAsync(request);
             }
         }
 
@@ -102,6 +114,28 @@
                 ContractResolver = new CamelCasePropertyNamesContractResolver()
             };
             return JsonConvert.SerializeObject(message, serializerSettings);
+        }
+
+        private class SendPushNotificationResult
+        {
+            public SendPushNotificationResult(IEnumerable<HttpResponseMessage> responses)
+            {
+                this.Responses = responses;
+            }
+
+            public IEnumerable<HttpResponseMessage> Responses { get; }
+        }
+
+        private class SendPushNotificationResultFinish
+        {
+            public static readonly SendPushNotificationResultFinish Instance = new SendPushNotificationResultFinish();
+
+            public SendPushNotificationResultFinish(string errorMessage = null)
+            {
+                this.ErrorMessage = errorMessage;
+            }
+
+            public string ErrorMessage { get; }
         }
     }
 }
