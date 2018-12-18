44d3ce440f9a1ab3c434bc96bb376f9330a12c21
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2018-12-18 03:42:11 +0300

    Added MVC controller for download application page.

---
+++ server/Arcadia.Assistant.Web/Controllers/DownloadWebController.cs
@@ -0,0 +1,29 @@
+﻿namespace Arcadia.Assistant.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using Arcadia.Assistant.Web.Configuration;
+    using Arcadia.Assistant.Web.Models;
+
+    [Route("/download")]
+    public class DownloadWebController : Controller
+    {
+        private readonly IDownloadApplicationSettings downloadApplicationSettings;
+
+        public DownloadWebController(IDownloadApplicationSettings downloadApplicationSettings)
+        {
+            this.downloadApplicationSettings = downloadApplicationSettings;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new DownloadWebModel
+            {
+                ApkDownloadPath = this.downloadApplicationSettings.ApkDownloadPath,
+                iPaDownloadPath = this.downloadApplicationSettings.IpaDownloadPath
+            };
+            return this.View(model);
+        }
+    }
+}
---
+++ server/Arcadia.Assistant.Web/Models/DownloadWebModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Arcadia.Assistant.Web.Models
+{
+    public class DownloadWebModel
+    {
+        public string ApkDownloadPath { get; set; }
+
+        public string iPaDownloadPath { get; set; }
+    }
+}
