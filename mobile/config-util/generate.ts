ab1a679dc6369818eb3ef6552f27086ec39af5ae
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2018-12-19 16:01:21 +0300

    writing env into a proper location

--- mobile/config-util/generate.ts
+++ mobile/config-util/generate.ts
@@ -23,7 +23,7 @@ interface Configuration {
 function createConfiguration(): Configuration {
     return {
         env: {
-            appCenterSecretId: process.env[''] || '',
+            appCenterSecretId: process.env['AA_API_APPCENTER'] || '<writeme>',
             apiUrl: process.env['AA_API_URL'] || 'https://arcadia-assistant-dev.arcadialab.ru/api',
             oauthRedirectUri: process.env['AA_OAUTH_REDIRECT_URI'] || 'arcadia-assistant://on-login',
             oauthClientId: process.env['AA_OAUTH_CLIENT_ID'] || 'a2ccb221-60e2-47b8-b28c-bf88a59f7f4a',
@@ -34,7 +34,7 @@ function createConfiguration(): Configuration {
 
 function writeEnvConfig(config: EnvConfiguration) {
     const data = Object.entries(config).map(x => `${x[0]}=${x[1]}`).join(EOL);
-    writeFileSync('.env', data);
+    writeFileSync('../.env', data);
 }
 
 export function run() {    
