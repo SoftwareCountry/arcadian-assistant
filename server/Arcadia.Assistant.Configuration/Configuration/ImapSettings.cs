f86aea6c7d6633fb4814f0a3b3cf8394662ed245
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2018-12-21 01:50:42 +0300

    Added imap settings.

---
+++ server/Arcadia.Assistant.Configuration/Configuration/ImapSettings.cs
@@ -0,0 +1,29 @@
+﻿namespace Arcadia.Assistant.Configuration.Configuration
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ImapSettings : IImapSettings
+    {
+        [Required]
+        public bool Enabled { get; set; }
+
+        [Required]
+        public string User { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Host { get; set; }
+
+        [Required]
+        public int Port { get; set; }
+
+        [Required]
+        public bool UseTls { get; set; }
+
+        [Required]
+        public int RefreshIntervalMinutes { get; set; }
+    }
+}
