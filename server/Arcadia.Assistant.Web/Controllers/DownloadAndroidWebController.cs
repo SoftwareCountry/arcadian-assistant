ff82428dda48cb6aa5b5bd76a33f01cf30504d97
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2019-01-30 16:56:56 +0300

    Refactored Download controller: divided it to several controllers to fit SRP.

---
+++ server/Arcadia.Assistant.Web/Controllers/DownloadAndroidWebController.cs
@@ -0,0 +1,14 @@
+﻿namespace Arcadia.Assistant.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class DownloadAndroidWebController : Controller
+    {
+        [Route("download/android")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
