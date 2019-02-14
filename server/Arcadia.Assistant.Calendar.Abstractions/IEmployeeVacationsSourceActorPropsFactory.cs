e567ae25bddf40c2b0913e87100918a4acaed5ce
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2019-02-14 14:25:17 +0300

    Added interface for employee vacations source actor props factory.

---
+++ server/Arcadia.Assistant.Calendar.Abstractions/IEmployeeVacationsSourceActorPropsFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace Arcadia.Assistant.Calendar.Abstractions
+{
+    using Akka.Actor;
+
+    public interface IEmployeeVacationsSourceActorPropsFactory
+    {
+        Props CreateProps(string employeeId);
+    }
+}
