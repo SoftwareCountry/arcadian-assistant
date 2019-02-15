7e0f8063f650da211b63a5d1f025058b945bd915
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2019-02-15 20:36:50 +0300

    Used employee vacations registry in employee vacations actor.

--- server/Arcadia.Assistant.Calendar/Vacations/EmployeeVacationsActor.cs
+++ server/Arcadia.Assistant.Calendar/Vacations/EmployeeVacationsActor.cs
@@ -1,44 +1,37 @@
 ﻿namespace Arcadia.Assistant.Calendar.Vacations
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
 
     using Akka.Actor;
     using Akka.Event;
-    using Akka.Persistence;
 
     using Arcadia.Assistant.Calendar.Abstractions;
     using Arcadia.Assistant.Calendar.Abstractions.EventBus;
     using Arcadia.Assistant.Calendar.Abstractions.Messages;
-    using Arcadia.Assistant.Calendar.Events;
     using Arcadia.Assistant.Feeds;
     using Arcadia.Assistant.Feeds.Messages;
     using Arcadia.Assistant.Organization.Abstractions;
 
-    public class EmployeeVacationsActor : CalendarEventsStorageBase
+    public class EmployeeVacationsActor : UntypedActor, ILogReceive
     {
+        private readonly string employeeId;
         private readonly IActorRef employeeFeed;
-
         private readonly IActorRef vacationsCreditRegistry;
-        private readonly IActorRef vacationsSource;
-
-        public override string PersistenceId { get; }
+        private readonly IActorRef vacationsRegistry;
 
         public EmployeeVacationsActor(
             string employeeId,
             IActorRef employeeFeed,
             IActorRef vacationsCreditRegistry,
-            IEmployeeVacationsSourceActorPropsFactory vacationsSourcePropsFactory
-        ) : base(employeeId, calendarEventsApprovalsChecker)
+            IEmployeeVacationsRegistryPropsFactory vacationsRegistryPropsFactory
+        )
         {
+            this.employeeId = employeeId;
             this.employeeFeed = employeeFeed;
             this.vacationsCreditRegistry = vacationsCreditRegistry;
-            this.vacationsSource = Context.ActorOf(
-                vacationsSourcePropsFactory.CreateProps(employeeId),
-                "vacations-source");
-
-            this.PersistenceId = $"employee-vacations-test-{this.EmployeeId}";
+            this.vacationsRegistry = Context.ActorOf(
+                vacationsRegistryPropsFactory.CreateProps(employeeId),
+                "vacations-registry");
 
             Context.System.EventStream.Subscribe<CalendarEventChanged>(this.Self);
         }
@@ -47,239 +40,60 @@
             string employeeId,
             IActorRef employeeFeed,
             IActorRef vacationsCreditRegistry,
-            IEmployeeVacationsSourceActorPropsFactory employeeVacationsSourceActorPropsFactory)
+            IEmployeeVacationsRegistryPropsFactory vacationsRegistryPropsFactory)
         {
             return Props.Create(() => new EmployeeVacationsActor(
                 employeeId,
                 employeeFeed,
                 vacationsCreditRegistry,
-                employeeVacationsSourceActorPropsFactory)
+                vacationsRegistryPropsFactory)
             );
         }
 
-        protected override void InsertCalendarEvent(
-            CalendarEvent calendarEvent,
-            string createdBy,
-            DateTimeOffset timestamp,
-            OnSuccessfulUpsertCallback onUpsert)
-        {
-            var eventId = calendarEvent.EventId;
-            var newEvent = new VacationIsRequested
-            {
-                EmployeeId = this.EmployeeId,
-                EventId = eventId,
-                StartDate = calendarEvent.Dates.StartDate,
-                EndDate = calendarEvent.Dates.EndDate,
-                TimeStamp = timestamp,
-                UserId = createdBy
-            };
-            this.Persist(newEvent, e =>
-            {
-                this.OnVacationRequested(e);
-                onUpsert(this.EventsById[eventId]);
-            });
-        }
-
-        protected override void OnCommand(object message)
+        protected override void OnReceive(object message)
         {
             switch (message)
             {
                 case CalendarEventChanged msg when
                     msg.NewEvent.Status == VacationStatuses.Approved &&
-                    msg.NewEvent.EmployeeId == this.EmployeeId:
+                    msg.NewEvent.EmployeeId == this.employeeId:
 
                     var text = $"Vacation approved from {msg.NewEvent.Dates.StartDate.ToLongDateString()} to {msg.NewEvent.Dates.EndDate.ToLongDateString()}";
-                    var feedMessage = new Message(Guid.NewGuid().ToString(), this.EmployeeId, "Vacation", text, msg.Timestamp.Date);
+                    var feedMessage = new Message(Guid.NewGuid().ToString(), this.employeeId, "Vacation", text, msg.Timestamp.Date);
                     this.employeeFeed.Tell(new PostMessage(feedMessage));
 
                     break;
 
                 case GetVacationsCredit _:
                     this.vacationsCreditRegistry
-                        .Ask<VacationsCreditRegistry.GetVacationInfo.Response>(new VacationsCreditRegistry.GetVacationInfo(this.EmployeeId))
+                        .Ask<VacationsCreditRegistry.GetVacationInfo.Response>(new VacationsCreditRegistry.GetVacationInfo(this.employeeId))
                         .ContinueWith(x => new GetVacationsCredit.Response(x.Result.VacationsCredit))
                         .PipeTo(this.Sender);
                     break;
 
+                case GetCalendarEvents msg:
+                    this.vacationsRegistry.Forward(msg);
+                    break;
+
+                case GetCalendarEvent msg:
+                    this.vacationsRegistry.Forward(msg);
+                    break;
+
+                case UpsertCalendarEvent msg:
+                    this.vacationsRegistry.Forward(msg);
+                    break;
+
+                case GetCalendarEventApprovals msg:
+                    this.vacationsRegistry.Forward(msg);
+                    break;
+
+                case ApproveCalendarEvent msg:
+                    this.vacationsRegistry.Forward(msg);
+                    break;
+
                 default:
-                    base.OnCommand(message);
+                    this.Unhandled(message);
                     break;
-            }
-        }
-
-        protected override void UpdateCalendarEvent(
-            CalendarEvent oldEvent,
-            string updatedBy,
-            DateTimeOffset timestamp,
-            CalendarEvent newEvent,
-            OnSuccessfulUpsertCallback onUpsert)
-        {
-            if (oldEvent.Dates != newEvent.Dates)
-            {
-                if (!oldEvent.IsPending)
-                {
-                    throw new Exception($"Date change is not allowed in status {oldEvent.Status} for {oldEvent.Type}");
-                }
-
-                if (this.ApprovalsByEvent.TryGetValue(oldEvent.EventId, out var approvals) && approvals.Count != 0)
-                {
-                    throw new Exception($"Date change is not allowed when there is at least one user approval for {oldEvent.Type}");
-                }
-
-                var eventToPersist = new VacationDatesAreEdited
-                {
-                    EventId = newEvent.EventId,
-                    StartDate = newEvent.Dates.StartDate,
-                    EndDate = newEvent.Dates.EndDate,
-                    TimeStamp = timestamp,
-                    UserId = updatedBy
-                };
-                this.Persist(eventToPersist, this.OnVacationDatesEdit);
-            }
-
-            if (oldEvent.Status != newEvent.Status)
-            {
-                switch (newEvent.Status)
-                {
-                    case VacationStatuses.Approved:
-                        this.Persist(new VacationIsApproved
-                        {
-                            EventId = newEvent.EventId,
-                            TimeStamp = timestamp,
-                            UserId = updatedBy
-                        }, this.OnVacationApproved);
-                        break;
-
-                    case VacationStatuses.Cancelled:
-                        this.Persist(new VacationIsCancelled
-                        {
-                            EventId = newEvent.EventId,
-                            TimeStamp = timestamp,
-                            UserId = updatedBy
-                        }, this.OnVacationCancel);
-                        break;
-
-                    case VacationStatuses.Rejected:
-                        this.Persist(new VacationIsRejected
-                        {
-                            EventId = newEvent.EventId,
-                            TimeStamp = timestamp,
-                            UserId = updatedBy
-                        }, this.OnVacationRejected);
-                        break;
-                }
-            }
-
-            onUpsert(newEvent);
-        }
-
-        protected override string GetInitialStatus()
-        {
-            return VacationStatuses.Requested;
-        }
-
-        protected override bool IsStatusTransitionAllowed(string oldCalendarEventStatus, string newCalendarEventStatus)
-        {
-            return VacationStatuses.All.Contains(newCalendarEventStatus)
-                && (oldCalendarEventStatus != VacationStatuses.Cancelled)
-                && (oldCalendarEventStatus != VacationStatuses.Rejected)
-                && (newCalendarEventStatus != this.GetInitialStatus())
-                && (newCalendarEventStatus != VacationStatuses.Approved);
-        }
-
-        protected override void OnRecover(object message)
-        {
-            switch (message)
-            {
-                case VacationIsRequested ev:
-                    this.OnVacationRequested(ev);
-                    break;
-
-                case VacationDatesAreEdited ev:
-                    this.OnVacationDatesEdit(ev);
-                    break;
-
-                case VacationIsRejected ev:
-                    this.OnVacationRejected(ev);
-                    break;
-
-                case VacationIsApproved ev:
-                    this.OnVacationApproved(ev);
-                    break;
-
-                case UserGrantedCalendarEventApproval ev:
-                    this.OnSuccessfulApprove(ev);
-                    break;
-
-                case VacationIsCancelled ev:
-                    this.OnVacationCancel(ev);
-                    break;
-
-                case RecoveryCompleted _:
-                    foreach (var @event in this.EventsById.Values)
-                    {
-                        if (@event.IsPending)
-                        {
-                            Context.System.EventStream.Publish(new CalendarEventRecoverComplete(@event));
-                        }
-                    }
-                    break;
-            }
-        }
-
-        private void OnVacationRequested(VacationIsRequested message)
-        {
-            var datesPeriod = new DatesPeriod(message.StartDate, message.EndDate);
-            var calendarEvent = new CalendarEvent(
-                message.EventId,
-                CalendarEventTypes.Vacation,
-                datesPeriod,
-                VacationStatuses.Requested,
-                this.EmployeeId);
-            this.EventsById[message.EventId] = calendarEvent;
-            this.ApprovalsByEvent[message.EventId] = new List<Approval>();
-        }
-
-        private void OnVacationDatesEdit(VacationDatesAreEdited message)
-        {
-            if (this.EventsById.TryGetValue(message.EventId, out var calendarEvent))
-            {
-                var newDates = new DatesPeriod(message.StartDate, message.EndDate);
-                this.EventsById[message.EventId] = new CalendarEvent(
-                    message.EventId,
-                    calendarEvent.Type,
-                    newDates,
-                    calendarEvent.Status,
-                    calendarEvent.EmployeeId);
-            }
-        }
-
-        private void OnVacationApproved(VacationIsApproved message)
-        {
-            if (this.EventsById.TryGetValue(message.EventId, out var calendarEvent))
-            {
-                this.EventsById[message.EventId] = new CalendarEvent(
-                    message.EventId,
-                    calendarEvent.Type,
-                    calendarEvent.Dates,
-                    VacationStatuses.Approved,
-                    calendarEvent.EmployeeId);
-            }
-        }
-
-        private void OnVacationCancel(VacationIsCancelled message)
-        {
-            if (this.EventsById.ContainsKey(message.EventId))
-            {
-                this.EventsById.Remove(message.EventId);
-            }
-        }
-
-        private void OnVacationRejected(VacationIsRejected message)
-        {
-            if (this.EventsById.ContainsKey(message.EventId))
-            {
-                this.EventsById.Remove(message.EventId);
             }
         }
     }
