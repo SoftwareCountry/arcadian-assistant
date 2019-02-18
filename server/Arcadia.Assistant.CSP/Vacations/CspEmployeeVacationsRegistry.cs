f00daf745d35a9ea5a35a983e0844cd492e0d882
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2019-02-19 02:13:04 +0300

    Used new method to periodically refresh vacations cache in CSP vacations registry.

--- server/Arcadia.Assistant.CSP/Vacations/CspEmployeeVacationsRegistry.cs
+++ server/Arcadia.Assistant.CSP/Vacations/CspEmployeeVacationsRegistry.cs
@@ -1,11 +1,14 @@
 ﻿namespace Arcadia.Assistant.CSP.Vacations
 {
     using System;
-
+    using System.Collections.Generic;
+    using System.Linq;
     using Akka.Actor;
     using Akka.Event;
-
+    using Arcadia.Assistant.Calendar.Abstractions;
+    using Arcadia.Assistant.Calendar.Abstractions.Messages;
     using Arcadia.Assistant.Configuration.Configuration;
+    using Arcadia.Assistant.CSP.Model;
 
     public class CspEmployeeVacationsRegistry : UntypedActor, ILogReceive
     {
@@ -13,6 +16,9 @@
         private readonly string employeeId;
 
         private readonly ILoggingAdapter logger = Context.GetLogger();
+
+        private Dictionary<string, CalendarEvent> eventsById = new Dictionary<string, CalendarEvent>();
+        private Dictionary<string, IEnumerable<Approval>> approvalsByEvent = new Dictionary<string, IEnumerable<Approval>>();
 
         public CspEmployeeVacationsRegistry(
             VacationsSyncExecutor vacationsSyncExecutor,
@@ -35,11 +41,29 @@
             switch (message)
             {
                 case Refresh _:
-                    this.logger.Info($"Updating vacations information for employee {this.employeeId}...");
+                    this.logger.Debug($"Updating vacations information for employee {this.employeeId}...");
+
+                    this.vacationsSyncExecutor.GetVacations(this.employeeId)
+                        .PipeTo(
+                            this.Self,
+                            success: result => new RefreshSuccess(result),
+                            failure: err => new RefreshFailed(err)
+                        );
+
                     break;
 
-                case RefreshSuccess _:
-                    this.logger.Info($"Vacations information for employee {this.employeeId} is updated");
+                case RefreshSuccess msg:
+                    this.logger.Debug($"Vacations information for employee {this.employeeId} is updated");
+
+                    this.eventsById.Clear();
+                    this.approvalsByEvent.Clear();
+
+                    foreach (var vacation in msg.Vacations)
+                    {
+                        this.eventsById[vacation.CalendarEvent.EventId] = vacation.CalendarEvent;
+                        this.approvalsByEvent[vacation.CalendarEvent.EventId] = vacation.Approvals;
+                    }
+
                     break;
 
                 case RefreshFailed msg:
@@ -55,6 +79,12 @@
 
         private class RefreshSuccess
         {
+            public RefreshSuccess(IReadOnlyCollection<CalendarEventWithApprovals> vacations)
+            {
+                this.Vacations = vacations;
+            }
+
+            public IReadOnlyCollection<CalendarEventWithApprovals> Vacations { get; }
         }
 
         private class RefreshFailed
