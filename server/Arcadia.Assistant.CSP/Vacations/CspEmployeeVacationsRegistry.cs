b8fb5bc898d16ec09e92cff351d75e05bc2b9d75
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2019-02-26 20:54:19 +0300

    Added employee id to logs about manually changed database.

--- server/Arcadia.Assistant.CSP/Vacations/CspEmployeeVacationsRegistry.cs
+++ server/Arcadia.Assistant.CSP/Vacations/CspEmployeeVacationsRegistry.cs
@@ -266,14 +266,14 @@
 
             foreach (var @event in createdEvents)
             {
-                this.logger.Debug($"Vacation with id {@event.CalendarEvent.EventId} was added to CSP database manually");
+                this.logger.Debug($"Vacation with id {@event.CalendarEvent.EventId} for employee {this.employeeId} was added to CSP database manually");
                 Context.System.EventStream.Publish(new CalendarEventCreated(@event.CalendarEvent, @event.CalendarEvent.EmployeeId, DateTimeOffset.Now));
                 this.databaseVacationsCache[@event.CalendarEvent.EventId] = @event;
             }
 
             foreach (var @event in updatedEvents)
             {
-                this.logger.Debug($"Vacation with id {@event.CalendarEvent.EventId} was updated in CSP database manually");
+                this.logger.Debug($"Vacation with id {@event.CalendarEvent.EventId} for employee {this.employeeId} was updated in CSP database manually");
                 var oldEvent = this.databaseVacationsCache[@event.CalendarEvent.EventId];
                 Context.System.EventStream.Publish(new CalendarEventChanged(oldEvent.CalendarEvent, @event.CalendarEvent.EmployeeId, DateTimeOffset.Now, @event.CalendarEvent));
 
@@ -282,14 +282,14 @@
 
             foreach (var @event in approvalsUpdatedEvents)
             {
-                this.logger.Debug($"Approvals for vacation with id {@event.CalendarEvent.EventId} was updated in CSP database manually");
+                this.logger.Debug($"Approvals for vacation with id {@event.CalendarEvent.EventId} for employee {this.employeeId} were updated in CSP database manually");
                 Context.System.EventStream.Publish(new CalendarEventApprovalsChanged(@event.CalendarEvent, @event.Approvals));
                 this.databaseVacationsCache[@event.CalendarEvent.EventId] = @event;
             }
 
             foreach (var @event in removedEvents)
             {
-                this.logger.Debug($"Vacation with id {@event.CalendarEvent.EventId} was removed from CSP database manually");
+                this.logger.Debug($"Vacation with id {@event.CalendarEvent.EventId} for employee {this.employeeId} was removed from CSP database manually");
                 // New event will be published here
             }
         }
