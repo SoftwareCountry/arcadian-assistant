b9d5d61b6c8c29553a8ebca20539354c0aa00b1e
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2019-03-22 20:43:45 +0300

    Added simple template expressions parser.

---
+++ server/Arcadia.Assistant.Calendar.Notifications/TemplateExpressionParser.cs
@@ -0,0 +1,14 @@
+﻿namespace Arcadia.Assistant.Calendar.Notifications
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class TemplateExpressionParser
+    {
+        public static string ParseTemplateExpression(this string template, IDictionary<string, string> context)
+        {
+            var result = context.Aggregate(template, (res, val) => res.Replace($"{{{val.Key}}}", val.Value));
+            return result;
+        }
+    }
+}
