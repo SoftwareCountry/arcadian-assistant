2c931a74cfd7bd734e426d019a91036ab0205094
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2019-04-19 15:49:42 +0300

    Added project for external storages abstractions.

---
+++ server/Arcadia.Assistant.ExternalStorages.Abstractions/IExternalStorage.cs
@@ -0,0 +1,17 @@
+﻿namespace Arcadia.Assistant.ExternalStorages.Abstractions
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface IExternalStorage
+    {
+        Task<IEnumerable<StorageItem>> GetItems(string list, IEnumerable<ICondition> conditions = null, CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<StorageItem> AddItem(string list, StorageItem item, CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<StorageItem> UpdateItem(string list, StorageItem item, IEnumerable<ICondition> conditions, CancellationToken cancellationToken = default(CancellationToken));
+
+        Task DeleteItem(string list, IEnumerable<ICondition> conditions, CancellationToken cancellationToken = default(CancellationToken));
+    }
+}
---
+++ server/Arcadia.Assistant.ExternalStorages.Abstractions/StorageItem.cs
@@ -0,0 +1,21 @@
+﻿namespace Arcadia.Assistant.ExternalStorages.Abstractions
+{
+    using System;
+
+    public class StorageItem
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public string Category { get; set; }
+
+        public string CalendarEventId { get; set; }
+    }
+}
