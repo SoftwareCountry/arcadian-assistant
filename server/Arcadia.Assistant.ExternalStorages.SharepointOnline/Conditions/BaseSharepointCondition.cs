7a58d5fc05e0a64af7130e3a2f2adb1a206fc35d
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2019-04-19 16:32:27 +0300

    Added Sharepoint equal condition.

---
+++ server/Arcadia.Assistant.ExternalStorages.SharepointOnline/Conditions/BaseSharepointCondition.cs
@@ -0,0 +1,29 @@
+﻿namespace Arcadia.Assistant.ExternalStorages.SharepointOnline.Conditions
+{
+    using System;
+    using System.Linq.Expressions;
+    using System.Reflection;
+
+    using Arcadia.Assistant.ExternalStorages.Abstractions;
+
+    public abstract class BaseSharepointCondition : ICondition
+    {
+        protected BaseSharepointCondition(Expression<Func<StorageItem, object>> property, object value)
+        {
+            // ToDo: Fix this
+            var propertyInfo = ((MemberExpression)property.Body).Member;
+
+            if (propertyInfo.MemberType != MemberTypes.Property)
+            {
+                throw new ArgumentException("Should be a property expression", nameof(property));
+            }
+
+            this.Property = property;
+            this.Value = value;
+        }
+
+        public Expression<Func<StorageItem, object>> Property { get; }
+
+        public object Value { get; }
+    }
+}
---
+++ server/Arcadia.Assistant.ExternalStorages.SharepointOnline/Conditions/SharepointEqualCondition.cs
@@ -0,0 +1,15 @@
+﻿namespace Arcadia.Assistant.ExternalStorages.SharepointOnline.Conditions
+{
+    using System;
+    using System.Linq.Expressions;
+
+    using Arcadia.Assistant.ExternalStorages.Abstractions;
+
+    public class SharepointEqualCondition : BaseSharepointCondition
+    {
+        public SharepointEqualCondition(Expression<Func<StorageItem, object>> property, object value)
+            : base(property, value)
+        {
+        }
+    }
+}
