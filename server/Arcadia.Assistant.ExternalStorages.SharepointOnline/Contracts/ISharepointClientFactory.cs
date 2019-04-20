bb89bc6ff83cb421ba37323434a9861aaeb82e3d
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2019-04-20 10:45:26 +0300

    Removed missing old Sharepoint classes.

--- server/Arcadia.Assistant.ExternalStorages.SharepointOnline/Contracts/ISharepointClientFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Arcadia.Assistant.ExternalStorages.SharepointOnline.Contracts
-{
-    using Microsoft.SharePoint.Client;
-
-    public interface ISharepointClientFactory
-    {
-        ClientContext GetClient(string url);
-    }
-}
