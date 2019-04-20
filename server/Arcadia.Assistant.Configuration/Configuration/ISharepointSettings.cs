bdc9677d44e9e3f8702787aac64a8664010d6d0a
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2019-04-20 10:45:09 +0300

    Added Sharepoint settings.

---
+++ server/Arcadia.Assistant.Configuration/Configuration/ISharepointSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace Arcadia.Assistant.Configuration.Configuration
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public interface ISharepointSettings
+    {
+        [Required]
+        string ClientId { get; }
+
+        [Required]
+        string ClientSecret { get; }
+
+        string CalendarEventIdMapping { get; }
+    }
+}
---
+++ server/Arcadia.Assistant.Configuration/Configuration/SharepointSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Arcadia.Assistant.Configuration.Configuration
+{
+    public class SharepointSettings : ISharepointSettings
+    {
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+
+        public string CalendarEventIdMapping { get; set; }
+    }
+}
