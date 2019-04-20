caf66fbebd993ad9431de60ac297e24bf2cc4aa6
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2019-04-21 01:12:50 +0300

    Removed one more missing old file.

--- server/Arcadia.Assistant.ExternalStorages.SharepointOnline/CustomTokenHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Arcadia.Assistant.ExternalStorages.SharepointOnline
-{
-    public static partial class TokenHelper
-    {
-        public static string ClientId { get; set; }
-
-        public static string ClientSecret { get; set; }
-    }
-}
