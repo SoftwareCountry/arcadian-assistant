13620b4e0eaea1b1949f1d83c3475fc37cb654ba
Yaroslav Tsimokha <yaroslav.tsimokha@arcadia.spb.ru>
2019-04-23 16:35:50 +0300

    Back option added to edit sick leave event dialog

--- mobile/src/calendar/event-dialog/edit-sick-leave-event-dialog.tsx
+++ mobile/src/calendar/event-dialog/edit-sick-leave-event-dialog.tsx
@@ -32,26 +32,36 @@ class EditSickLeaveEventDialogImpl extends Component<EditSickLeaveEventDialogPro
     //----------------------------------------------------------------------------
     public render() {
         const selectedSickLeave = this.selectedSickLeave();
-        const isCompleted = selectedSickLeave ? !selectedSickLeave.calendarEvent.isCompleted : false;
 
+        const isCompleted = selectedSickLeave ? !selectedSickLeave.calendarEvent.isCompleted : false;
         const isDisabledForComplete = this.props.completeSickLeavePermission === false || isCompleted;
-        const acceptLabel = isDisabledForComplete ? undefined : 'Complete';
-        
-        return <EventDialogBase
-            title={'Hey! Hope you feel better'}
-            text={this.text}
-            icon={'sick_leave'}
-            cancelLabel={'Prolong'}
-            disableCancel={!isCompleted}
-            onCancelPress={this.cancelAction}
-            acceptLabel={acceptLabel}
-            disableAccept={isDisabledForComplete}
-            onAcceptPress={this.acceptAction}
-            onClosePress={this.closeDialog}/>;
+
+        if (isDisabledForComplete) {
+            return <EventDialogBase
+                title={'Hey! Hope you feel better'}
+                text={this.text}
+                icon={'sick_leave'}
+                cancelLabel={'Back'}
+                onCancelPress={this.closeDialog}
+                acceptLabel={'Prolong'}
+                onAcceptPress={this.prolong}
+                onClosePress={this.closeDialog}/>;
+        } else {
+            return <EventDialogBase
+                title={'Hey! Hope you feel better'}
+                text={this.text}
+                icon={'sick_leave'}
+                cancelLabel={'Prolong'}
+                disableCancel={!isCompleted}
+                onCancelPress={this.prolong}
+                acceptLabel={'Complete'}
+                onAcceptPress={this.complete}
+                onClosePress={this.closeDialog}/>;
+        }
     }
 
     //----------------------------------------------------------------------------
-    private cancelAction = () => {
+    private prolong = () => {
         const selectedSickLeave = this.selectedSickLeave();
         if (!selectedSickLeave) {
             return;
@@ -61,7 +71,7 @@ class EditSickLeaveEventDialogImpl extends Component<EditSickLeaveEventDialogPro
     };
 
     //----------------------------------------------------------------------------
-    private acceptAction = () => {
+    private complete = () => {
         const selectedSickLeave = this.selectedSickLeave();
         if (!selectedSickLeave) {
             return;
