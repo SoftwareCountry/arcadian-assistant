c57ef8cbef24beeefb1513e63d90f9b60a893171
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2019-04-29 18:16:18 +0300

    Replaced using of database view with direct query to override some conditions.

--- server/Arcadia.Assistant.CSP/CspEmployeeQuery.cs
+++ server/Arcadia.Assistant.CSP/CspEmployeeQuery.cs
@@ -10,7 +10,14 @@
     {
         private readonly ArcadiaCspContext ctx;
 
-        public const string ArcadianEmployeeQuery = "SELECT * FROM dbo.ArcadianEmployee WHERE IsDelete <> 1";
+        public const string ArcadianEmployeeQuery = @"
+            SELECT 
+                Id, FirstName, MiddleName, LastName, FirstNameRus, MiddleNameRus, LastNameRus, LoginName, SID, Birthday, BusinessPhone, HomePhone, Email,
+                DepartmentId, PositionId, HiringDate, FiringDate, IsWorking, Description, IsPartTime, Image, CompanyId, IsInStaff, IntrabaseId, Gender,
+                ClockNumber, RoomNumber, BusinessCountry, BusinessZIP, BusinessCity, BusinessStreet, BusinessStreet2, BusinessStreet3, BusinessPhone2,
+                HomeCountry, HomeZIP, HomeCity, HomeStreet, HomeStreet2, HomeStreet3, MobilePhone, ProbationEnd, WeekHours, PartTime, IsDelete
+            FROM dbo.Employee
+            WHERE (FiringDate IS NULL) AND (IsDelete <> 1) AND (CompanyId = 154) OR (Id = 145)";
 
         public CspEmployeeQuery(ArcadiaCspContext ctx)
         {
