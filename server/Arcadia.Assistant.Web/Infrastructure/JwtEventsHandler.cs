a0b13b831371e8daa763c54350dd09097ea18d7c
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2019-04-29 18:54:17 +0300

    Added more authentication traces.

--- server/Arcadia.Assistant.Web/Infrastructure/JwtEventsHandler.cs
+++ server/Arcadia.Assistant.Web/Infrastructure/JwtEventsHandler.cs
@@ -10,6 +10,12 @@
     {
         private readonly ILogger logger = LogManager.GetLogger("Auth");
 
+        public override Task TokenValidated(TokenValidatedContext context)
+        {
+            this.logger.Warn($"JWT token validated. User identity: {context.Principal.Identity.Name}");
+            return base.TokenValidated(context);
+        }
+
         public override Task AuthenticationFailed(AuthenticationFailedContext context)
         {
             this.logger.Error(context.Exception, "JWT Error occurred");
