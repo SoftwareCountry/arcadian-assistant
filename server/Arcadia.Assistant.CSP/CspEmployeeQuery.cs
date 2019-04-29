82070d3a3c67fb1626ad02ef943c0407d0b14673
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2019-04-29 18:49:37 +0300

    Replaced SQL query with EF LINQ query.

--- server/Arcadia.Assistant.CSP/CspEmployeeQuery.cs
+++ server/Arcadia.Assistant.CSP/CspEmployeeQuery.cs
@@ -10,15 +10,6 @@
     {
         private readonly ArcadiaCspContext ctx;
 
-        public const string ArcadianEmployeeQuery = @"
-            SELECT 
-                Id, FirstName, MiddleName, LastName, FirstNameRus, MiddleNameRus, LastNameRus, LoginName, SID, Birthday, BusinessPhone, HomePhone, Email,
-                DepartmentId, PositionId, HiringDate, FiringDate, IsWorking, Description, IsPartTime, Image, CompanyId, IsInStaff, IntrabaseId, Gender,
-                ClockNumber, RoomNumber, BusinessCountry, BusinessZIP, BusinessCity, BusinessStreet, BusinessStreet2, BusinessStreet3, BusinessPhone2,
-                HomeCountry, HomeZIP, HomeCity, HomeStreet, HomeStreet2, HomeStreet3, MobilePhone, ProbationEnd, WeekHours, PartTime, IsDelete
-            FROM dbo.Employee
-            WHERE (FiringDate IS NULL) AND (IsDelete <> 1) AND (CompanyId = 154) OR (Id = 145)";
-
         public CspEmployeeQuery(ArcadiaCspContext ctx)
         {
             this.ctx = ctx;
@@ -26,7 +17,9 @@
 
         public IQueryable<Employee> Get()
         {
-            return this.ctx.Employee.FromSql(ArcadianEmployeeQuery);
+            return this.ctx.Employee
+                .AsNoTracking()
+                .Where(x => x.FiringDate == null && !x.IsDelete && x.CompanyId == 154 || x.Id == 145); // Don't really know what these constants are about, just copied from ArcadianEmployee view
         }
     }
 }
