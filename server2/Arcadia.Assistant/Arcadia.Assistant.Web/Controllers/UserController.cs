4b3e341a4980ed88e2897f8e4e903c800104a273
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2019-06-18 13:33:49 +0300

    Employee permissions model added

--- server2/Arcadia.Assistant/Arcadia.Assistant.Web/Controllers/UserController.cs
+++ server2/Arcadia.Assistant/Arcadia.Assistant.Web/Controllers/UserController.cs
@@ -7,7 +7,10 @@
     using Employees.Contracts;
 
     using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
+
+    using Models;
 
     [Route("api/user")]
     [Authorize]
@@ -21,6 +24,8 @@
         }
 
         [HttpGet]
+        [ProducesResponseType(typeof(UserModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetCurrentUser(CancellationToken cancellationToken)
         {
             var userEmployees = await this.employees.FindEmployeesAsync(EmployeesQuery.Create().WithIdentity(this.User.Identity.Name), cancellationToken);
@@ -30,7 +35,21 @@
                 return this.Forbid();
             }
 
-            return this.Ok(employee);
+            return this.Ok(new UserModel()
+            {
+                EmployeeId = employee.EmployeeId,
+                Username = employee.Name
+            });
+        }
+
+        [Route("permissions/{employeeId}")]
+        [HttpGet]
+        [ProducesResponseType(typeof(UserEmployeePermissionsModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetPermissions(string employeeId, CancellationToken token)
+        {
+            return null;
         }
     }
 }
---
+++ server2/Arcadia.Assistant/Arcadia.Assistant.Web/Models/UserEmployeePermissionsModel.cs
@@ -0,0 +1,54 @@
+﻿namespace Arcadia.Assistant.Web.Models
+{
+    using System;
+    using System.Linq;
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class UserEmployeePermissionsModel
+    {
+        [DataMember]
+        public string EmployeeId { get; }
+
+        [DataMember]
+        public string[] PermissionsNames { get; }
+
+        public UserEmployeePermissionsModel(string employeeId, EmployeePermissionsEntry employeePermissionsEntry)
+        {
+            this.EmployeeId = employeeId;
+            this.PermissionsNames = this.ExtractPermissionNames(employeePermissionsEntry);
+        }
+
+        private string[] ExtractPermissionNames(EmployeePermissionsEntry employeePermissionsEntry)
+        {
+            return employeePermissionsEntry
+                .ToString("G")
+                .Split(", ")
+                .Select(x =>
+                {
+                    var camelCased = char.ToLowerInvariant(x[0]) + x.Substring(1);
+                    return camelCased;
+                })
+                .ToArray();
+        }
+    }
+
+    [Flags]
+    public enum EmployeePermissionsEntry
+    {
+        None = 0,
+        ReadEmployeeInfo = 1 << 0,
+        ReadEmployeeVacationsCounter = 1 << 1,
+        ReadEmployeeDayoffsCounter = 1 << 2,
+        ReadEmployeePhone = 1 << 3,
+        ReadEmployeeCalendarEvents = 1 << 4,
+        CreateCalendarEvents = 1 << 5,
+        ApproveCalendarEvents = 1 << 6,
+        RejectCalendarEvents = 1 << 7,
+        CompleteSickLeave = 1 << 8,
+        ProlongSickLeave = 1 << 9,
+        CancelPendingCalendarEvents = 1 << 10,
+        EditPendingCalendarEvents = 1 << 11,
+        CancelApprovedCalendarEvents = 1 << 12
+    }
+}
