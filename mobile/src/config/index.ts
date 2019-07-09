86a3758b425e52087f548d00bacd0e26a1a724a4
Yaroslav Tsimokha <yaroslav.tsimokha@arcadia.spb.ru>
2019-07-09 12:10:48 +0300

    Download new version link for android changed

--- mobile/src/config/index.ts
+++ mobile/src/config/index.ts
@@ -5,7 +5,7 @@ const config = Object.freeze({
 
     baseUrl: Config.baseUrl as string,
     apiUrl: `${Config.baseUrl}/api`,
-    downloadNewVersionLink: Platform.OS === 'android' ? `${Config.baseUrl}/get/android` :
+    downloadNewVersionLink: Platform.OS === 'android' ? `${Config.baseUrl}/download/android` :
         `itms-services://?action=download-manifest&url=${Config.baseUrl}/download/ios-manifest`,
 
     oauth: Object.freeze({
