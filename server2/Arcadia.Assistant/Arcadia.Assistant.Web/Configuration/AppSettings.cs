b59bde2065714185215d62488200457695c6f126
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2019-12-03 17:09:07 +0300

    Got rid of nested classes

--- server2/Arcadia.Assistant/Arcadia.Assistant.Web/Configuration/AppSettings.cs
+++ server2/Arcadia.Assistant/Arcadia.Assistant.Web/Configuration/AppSettings.cs
@@ -4,16 +4,6 @@
     {
         public ConfigPackageSettings Config { get; set; } = new ConfigPackageSettings();
 
-        public class LinkConfiguration : IHelpSettings
-        {
-            public string HelpLink { get; set; } = string.Empty;
-        }
-
-        public class SslConfiguration : ISslSettings
-        {
-            public bool SslOffloading { get; set; } = false;
-        }
-
         public class ConfigPackageSettings
         {
             public SecuritySettings Security { get; set; } = new SecuritySettings();
---
+++ server2/Arcadia.Assistant/Arcadia.Assistant.Web/Configuration/LinkConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace Arcadia.Assistant.Web.Configuration
+{
+    public class LinkConfiguration : IHelpSettings
+    {
+        public string HelpLink { get; set; } = string.Empty;
+    }
+}
