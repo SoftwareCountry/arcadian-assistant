18090d22dc9cf2c39f592b384921a1089a1d0de1
Artem Kirienko <artem.kirienko@gmail.com>
2020-01-13 21:19:19 +0300

    Auth in RN 0.59 fixed

--- mobile/src/index.ts
+++ mobile/src/index.ts
@@ -1,6 +1,5 @@
 import { AppRegistry } from 'react-native';
 import { Root } from './root';
-import 'react-native-url-polyfill/auto';
 
 const defaultHandler = ErrorUtils.getGlobalHandler && ErrorUtils.getGlobalHandler();
 ErrorUtils.setGlobalHandler(function (error: any, isFatal: boolean = true) {
