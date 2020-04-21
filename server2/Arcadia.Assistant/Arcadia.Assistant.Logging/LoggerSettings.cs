6bb1d5d77b70645dc237c9bbd1ae48929001522d
sergey.shilov <>
2020-04-21 11:28:44 +0300

    Add Serilog to logger project

--- server2/Arcadia.Assistant/Arcadia.Assistant.Logging/LoggerSettings.cs
+++ server2/Arcadia.Assistant/Arcadia.Assistant.Logging/LoggerSettings.cs
@@ -1,10 +1,13 @@
 ﻿namespace Arcadia.Assistant.Logging
 {
+    using System;
     using System.Fabric.Description;
 
     public class LoggerSettings
     {
         public const string ApplicationInsightsConfigurationKey = "ApplicationInsightsKey";
+        public const string SerilogConsoleConfigurationKey = "LogToConsole";
+        public const string SerilogFileNameConfigurationKey = "LogFileName";
 
         public LoggerSettings()
         {
@@ -13,6 +16,8 @@
         public LoggerSettings(ConfigurationSection configurationSection)
         {
             this.ApplicationInsightsKey = configurationSection.Parameters[ApplicationInsightsConfigurationKey].Value;
+            this.FileLoggingFileName = configurationSection.Parameters[SerilogFileNameConfigurationKey].Value;
+            this.ConsoleLoggingOption = Boolean.TryParse(configurationSection.Parameters[SerilogConsoleConfigurationKey].Value, out var value) && value;
         }
 
         public LoggerSettings(string appKey)
@@ -21,6 +26,10 @@
         }
 
         public string ApplicationInsightsKey { get; set; } = string.Empty;
+
+        public bool ConsoleLoggingOption { get; set; }
+
+        public string FileLoggingFileName { get; set; } = string.Empty;
 
         public static LoggerSettings FromInsightsKey(string applicationInsightsKey)
         {
